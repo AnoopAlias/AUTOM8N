3cb8ed12aa2d7704c1c951566c87910b9baa3b18
Anoop P Alias <anoopalias01@gmail.com>
2014-08-13 09:31:19 -0400

    13 Aug 2014 7 PM IST

--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -14,9 +14,28 @@ yaml_parsed = yaml.safe_load(data_stream)
 
 
 main_domain = yaml_parsed.get('main_domain')   
-parked_domains = yaml_parsed.get('parked_domains')
+parked_domains = yaml_parsed.get('parked_domains')   #This data is irrelevant as parked domain list can be obtained from the domains userdata
 addon_domains = yaml_parsed.get('addon_domains')     #This data is irrelevant as addon is mapped to a subdomain
 sub_domains = yaml_parsed.get('sub_domains')
+
+def nginx_confgen(user_name,domain_name,config_template_code):
+	"Function that generates nginx config given a domain name"
+	cpdomainyaml = "/var/cpanel/userdata/"+user_name+"/"+domain_name
+	data_dstream = open(cpdomainyaml,'r')
+	yaml_dparsed = yaml.safe_load(data_dstream)
+	template_file = open("../conf/"+config_template_code+".tmpl",'r')
+	config_out = open("../sites-enabled/"+domain_name+".conf",'w')
+	for line in template_file:
+		config_out.write(line.replace('CPANELIP',cpanel_ipv4))
+		config_out.write(line.replace('DOMAINNAME',domain_name))
+	template_file.close()
+	config_out.close()
+
+
+
+
+
+
 
 print main_domain
 ##nginx_confgen("domain_name")
