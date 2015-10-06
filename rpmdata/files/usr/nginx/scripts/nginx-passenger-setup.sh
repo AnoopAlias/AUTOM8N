79182f7cf083c30ca9665b3851709fa1381fdcd6
root <root@cpanel.sysally.net>
2015-10-06 01:42:37 -0400

    php-fpm socket security fix

--- rpmdata/files/usr/nginx/scripts/nginx-passenger-setup.sh
+++ rpmdata/files/usr/nginx/scripts/nginx-passenger-setup.sh
@@ -7,6 +7,7 @@ if [ ! -d /usr/local/rvm/gems/ruby-RUBY_VERSION ] ; then
 fi
 if [ ! -d /usr/local/rvm/gems/ruby-RUBY_VERSION/gems/passenger-PASSENGER_VERSION/ext/nginx ] ; then
 	. /usr/local/rvm/scripts/rvm
+	rvm use ruby-RUBY_VERSION
         /usr/local/rvm/rubies/ruby-RUBY_VERSION/bin/gem install passenger -v PASSENGER_VERSION
         ln -s /usr/nginx/buildout /usr/local/rvm/gems/ruby-RUBY_VERSION/gems/passenger-PASSENGER_VERSION/
 fi
