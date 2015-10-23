a0647c106c1578395485e6fcc3ed6f5fc132b711
Anoop P Alias <anoop.alias@piserve.com>
2015-10-23 15:01:00 +0530

    nginx rpm script support for in-place upgrades

--- rpm_buildtree/nDeploy-nginx_build_script.sh
+++ rpm_buildtree/nDeploy-nginx_build_script.sh
@@ -62,4 +62,4 @@ cd ../nginx-pkg-64
 mkdir -p var/cache/nginx/ngx_pagespeed
 mkdir -p var/log/nginx
 mkdir -p var/run
-fpm -s dir -t rpm -C ../nginx-pkg-64 --vendor "PiServe Technologies" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m info@piserve.com -e --description "nDeploy custom nginx package" --url http://piserve.com --conflicts nginx -d zlib -d openssl -d pcre -d libcurl --after-install ../after_nginx_install --name nginx-nDeploy .
+fpm -s dir -t rpm -C ../nginx-pkg-64 --vendor "PiServe Technologies" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m info@piserve.com -e --description "nDeploy custom nginx package" --url http://piserve.com --conflicts nginx -d zlib -d openssl -d pcre -d libcurl --after-install ../after_nginx_install --before-remove ../after_nginx_uninstall --name nginx-nDeploy .
