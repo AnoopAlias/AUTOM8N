12028d9e239cd9e7cc49b8e73ae2a4fed54aec8b
Anoop P Alias <anoop.alias@piserve.com>
2015-10-26 00:20:13 +0530

    Added GeoIP python setup to NXAPI setup script

--- rpm_buildtree/nginx-pkg-64-common/usr/nginx/scripts/nxapi-setup.sh
+++ rpm_buildtree/nginx-pkg-64-common/usr/nginx/scripts/nxapi-setup.sh
@@ -3,4 +3,5 @@
 yum -y install python-pip
 pip install --upgrade pip
 pip install -r /usr/nginx/nxapi/requirements.txt
+yum -y install python-GeoIP.x86_64 python-geoip-geolite2.noarch python-pygeoip.noarch
 echo "NXAPI setup in /usr/nginx/nxapi//usr/nginx/nxapi/"
