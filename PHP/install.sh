2df4b8ec85cecd11956d7d710cbb722e940dd1a6
pumamd <pumamd@yandex.ru>
2015-11-07 11:44:24 +0200

    * fixes

--- PHP/install.sh
+++ PHP/install.sh
@@ -2,6 +2,7 @@
 
 yum install libmcrypt-devel openssl-devel gmp-devel curl-devel libcurl-devel libcurlssl-devel -y
 yum install firebird-devel -y
+yum install ImageMagick-devel sqlite-devel freetds-devel
 
 curl -L -O https://github.com/phpbrew/phpbrew/raw/master/phpbrew
 chmod +x phpbrew
@@ -23,32 +24,42 @@ source ~/.phpbrew/bashrc
 export PHPBREW_ROOT=/usr/local/phpbrew
 mkdir $PHPBREW_ROOT -v
 
-php -n /usr/bin/phpbrew --debug install --jobs 12 --patch fpm-lve-php5.4_fixed.patch 5.6.15 +default +fpm +mysql +exif +ftp +gd +intl +soap +pdo +curl +gmp +imap +opcache -- --with-libdir=lib64 --with-gd=shared --enable-gd-natf --with-jpeg-dir=/usr --with-png-dir=/usr
-php -n /usr/bin/phpbrew --debug install --jobs 12 --patch fpm-lve-php5.4_fixed.patch 5.5.30 +default +fpm +mysql +exif +ftp +gd +intl +soap +pdo +curl +gmp +imap -- --with-libdir=lib64 --with-gd=shared --enable-gd-natf --with-jpeg-dir=/usr --with-png-dir=/usr
-php -n /usr/bin/phpbrew --debug install --jobs 12 --patch fpm-lve-php5.4_fixed.patch 5.4.45 +default +fpm +mysql +exif +ftp +gd +intl +soap +pdo +curl +gmp +imap -- --with-libdir=lib64 --with-gd=shared --enable-gd-natf --with-jpeg-dir=/usr --with-png-dir=/usr
-php -n /usr/bin/phpbrew --debug install --jobs 12 --patch fpm-lve-php5.4_fixed.patch 5.3.29 +default +fpm +mysql +exif +ftp +gd +intl +soap +pdo +curl +gmp +imap -- --with-libdir=lib64 --with-gd=shared --enable-gd-natf --with-jpeg-dir=/usr --with-png-dir=/usr
+default +fpm +mysql +exif +ftp +gd +intl +soap +pdo +curl +gmp +imap +iconv +sqlite +gettext
+php -n /usr/bin/phpbrew --debug install --jobs 12 --patch fpm-lve-php5.4_fixed.patch 5.6.15 +default +fpm +mysql +exif +ftp +gd +intl +soap +pdo +curl +gmp +imap +iconv +sqlite +gettext -- --with-libdir=lib64 --with-gd=shared --enable-gd-natf --with-jpeg-dir=/usr --with-png-dir=/usr
+php -n /usr/bin/phpbrew --debug install --jobs 12 --patch fpm-lve-php5.4_fixed.patch 5.5.30 +default +fpm +mysql +exif +ftp +gd +intl +soap +pdo +curl +gmp +imap +iconv +sqlite +gettext -- --with-libdir=lib64 --with-gd=shared --enable-gd-natf --with-jpeg-dir=/usr --with-png-dir=/usr
+php -n /usr/bin/phpbrew --debug install --jobs 12 --patch fpm-lve-php5.4_fixed.patch 5.4.45 +default +fpm +mysql +exif +ftp +gd +intl +soap +pdo +curl +gmp +imap +iconv +sqlite +gettext -- --with-libdir=lib64 --with-gd=shared --enable-gd-natf --with-jpeg-dir=/usr --with-png-dir=/usr
+php -n /usr/bin/phpbrew --debug install --jobs 12 --patch fpm-lve-php5.4_fixed.patch 5.3.29 +default +fpm +mysql +exif +ftp +gd +intl +soap +pdo +curl +gmp +imap +iconv +sqlite +gettext -- --with-libdir=lib64 --with-gd=shared --enable-gd-natf --with-jpeg-dir=/usr --with-png-dir=/usr
 
 
-phpbrew use php-5.4.42
+phpbrew use php-5.4.45
+
+EXTENSIONS="imagick pdo_firebird memcache uploadprogress"
+for i in $EXTENSIONS; do
+  phpbrew ext install $i
+  if [ $? -ne 0 ]; then
+    exit $?
+  fi
+  phpbrew ext enable $i
+done
 
 phpbrew ext install imagick
-phpbrew ext install iconv
-phpbrew ext install pdo_sqlite
-phpbrew ext install pdo_dblib
+phpbrew ext install pdo_dblib -- --with-libdir=lib64
 phpbrew ext install pdo_firebird
-# phpbrew ext install curl
-phpbrew ext install memcached
-phpbrew ext install memcache
-phpbrew ext install gd
+phpbrew ext install memcache /memcached
 phpbrew ext install uploadprogress
-phpbrew ext install sqlite3
+
+# devel
 phpbrew ext install xhprof 0.9.4
 phpbrew ext disable xhprof
 
-# for bnpmd
-phpbrew ext install gettext
+# gd installed by main php compile
+mkdir /usr/local/phpbrew/php/$PHPBREW_PHP/var/db/ -p
+echo extension=gd.so >> /usr/local/phpbrew/php/$PHPBREW_PHP/var/db/gd.ini
+phpbrew ext enable gd
+phpbrew ext install gd -- --with-freetype-dir=/usr/include/freetype ???? not work
 
 http://www.directadmin.com/imap.txt
+https://github.com/phpbrew/phpbrew/issues/227
 yum install libc-client-devel uw-imap.x86_64
 phpbrew ext install imap
 
