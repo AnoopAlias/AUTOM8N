9951aa59cecd452e45e30189d9d0aef980137a81
Anoop P Alias <anoop.alias@piserve.com>
2015-11-08 10:20:46 +0530

    bild script extending + naxsi learning script modify(omitting | in rules)

--- rpm_buildtree/nDeploy_build_script.sh
+++ rpm_buildtree/nDeploy_build_script.sh
@@ -17,9 +17,11 @@ rsync -av ../nDeploy_cp/ nDeploy-pkg-centos7/opt/nDeploy/nDeploy_cp/
 cd nDeploy-pkg
 mkdir opt/nDeploy/lock
 fpm -s dir -t rpm -C ../nDeploy-pkg --vendor "PiServe Technologies" --iteration ${RPM_ITERATION}.el6 -d python-inotify -d nginx-nDeploy -d python-argparse -d PyYAML -d python-lxml -a noarch -m info@piserve.com -e --description "nDeploy cPanel plugin" --url http://piserve.com --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
+cp nDeploy-* /home/rpmrepo/public_html/CentOS/6/x86_64/
 
 cd ..
 cd nDeploy-pkg-centos7
 mkdir opt/nDeploy/lock
 fpm -s dir -t rpm -C ../nDeploy-pkg-centos7 --vendor "PiServe Technologies" --iteration ${RPM_ITERATION}.el7 -d python-inotify -d nginx-nDeploy -d python-argparse -d PyYAML -d python-lxml -a noarch -m info@piserve.com -e --description "nDeploy cPanel plugin" --url http://piserve.com --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
+cp nDeploy-* /home/rpmrepo/public_html/CentOS/7/x86_64/
 cd ..
