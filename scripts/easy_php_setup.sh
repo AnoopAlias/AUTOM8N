3c3265540e896c7898eedbbb259f44aea867e3de
Anoop P Alias <anoop.alias@piserve.com>
2015-12-04 17:57:06 +0530

    Support for php70 in easy_php script +misc bugs

--- scripts/easy_php_setup.sh
+++ scripts/easy_php_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #Author: Anoop P Alias
-if [ $(uname -r|grep lve) ];then
+
+#Function defs
+
+setup_lve_patched_php(){
 	wget -O /tmp/phpversion.hint http://rpm.piserve.com/phpversion.hint
 
 	#Compile PHP54
@@ -71,6 +74,7 @@ if [ $(uname -r|grep lve) ];then
 	compile_php54
 	compile_php55
 	compile_php56
+	#compile_php70  #As an when its available
 
 
 
@@ -87,8 +91,10 @@ if [ $(uname -r|grep lve) ];then
 	else
 		echo "ndeploy_backends not restarted"
 	fi
-else
-	if [ -f /etc/cpanel/ea4/is_ea4 ];then
+}
+
+
+setup_ea4_php(){
 		yum -y install ea-php54-php-fpm ea-php55-php-fpm ea-php56-php-fpm
 		for ver in 54 55 56
 		do
@@ -104,19 +110,31 @@ else
 			service ndeploy_backends start || systemctl start ndeploy_backends
 			chkconfig ndeploy_backends on || systemctl enable ndeploy_backends
 		done
-	else
+	}
+
+setup_remi_php(){
 		yum -y install scl-utils libmcrypt
 		osversion=$(cat /etc/redhat-release | grep -oE '[0-9]+\.[0-9]+'|cut -d"." -f1)
 		echo ${osversion}
 		if [ ${osversion} -eq 6 ];then
 			yum -y install http://rpms.remirepo.net/enterprise/remi-release-6.rpm
-			for ver in 54 55 56
+			for ver in 54 55 56 70
 			do
 				yum -y --disableexcludes=main --enablerepo=remi install php$ver php$ver-php-fpm php$ver-php-opcache php$ver-php-mysqlnd php$ver-php-gd php$ver-php-imap php$ver-php-intl php$ver-php-ioncube-loader php$ver-php-xmlrpc php$ver-php-xml php$ver-php-mcrypt php$ver-php-mbstring php$ver-php-ioncube-loader php$ver-php-intl php$ver-php-imap php$ver-php-gd
 				sed "s/CPANELUSER/nobody/g" /opt/nDeploy/conf/php-fpm.pool.tmpl > /opt/remi/php$ver/root/etc/php-fpm.d/www.conf
 				ln -s /opt/remi/php$ver/root/usr/sbin/php-fpm /opt/remi/php$ver/root/sbin/php-fpm
-				ln -s /opt/remi/php$ver/root/etc/php-fpm.d /opt/remi/php$ver/root/usr/etc/php-fpm.d
-				ln -s /opt/remi/php$ver/root/var /opt/remi/php$ver/root/usr/var
+				if [ ! -d /opt/remi/php$ver/root/var ];then
+					ln -s /var/opt/remi/php$ver /opt/remi/php$ver/root/var
+					ln -s /var/opt/remi/php$ver /opt/remi/php$ver/root/usr/var
+				else
+					ln -s /opt/remi/php$ver/root/var /opt/remi/php$ver/root/usr/
+				fi
+				if [ ! -d /opt/remi/php$ver/root/etc ];then
+					ln -s /etc/opt/remi/php$ver /opt/remi/php$ver/root/etc
+					ln -s /etc/opt/remi/php$ver /opt/remi/php$ver/root/usr/etc
+				else
+					ln -s /opt/remi/php$ver/root/etc/php-fpm.d /opt/remi/php$ver/root/usr/etc/
+				fi
 				/opt/nDeploy/scripts/update_backend.py PHP PHP$ver /opt/remi/php$ver/root
 				/etc/init.d/ndeploy_backends stop
 				/etc/init.d/ndeploy_backends start
@@ -124,13 +142,23 @@ else
 			done
 		elif [ ${osversion} -eq 7 ];then
 			yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-			for ver in 54 55 56
+			for ver in 54 55 56 70
 			do
 				yum -y --disableexcludes=main --enablerepo=remi install php$ver php$ver-php-fpm php$ver-php-opcache php$ver-php-mysqlnd php$ver-php-gd php$ver-php-imap php$ver-php-intl php$ver-php-ioncube-loader php$ver-php-xmlrpc php$ver-php-xml php$ver-php-mcrypt php$ver-php-mbstring php$ver-php-ioncube-loader php$ver-php-intl php$ver-php-imap php$ver-php-gd
 				sed "s/CPANELUSER/nobody/g" /opt/nDeploy/conf/php-fpm.pool.tmpl > /opt/remi/php$ver/root/etc/php-fpm.d/www.conf
 				ln -s /opt/remi/php$ver/root/usr/sbin/php-fpm /opt/remi/php$ver/root/sbin/php-fpm
-				ln -s /opt/remi/php$ver/root/etc/php-fpm.d /opt/remi/php$ver/root/usr/etc/php-fpm.d
-				ln -s /opt/remi/php$ver/root/var /opt/remi/php$ver/root/usr/var
+				if [ ! -d /opt/remi/php$ver/root/var ];then
+					ln -s /var/opt/remi/php$ver /opt/remi/php$ver/root/var
+					ln -s /var/opt/remi/php$ver /opt/remi/php$ver/root/usr/var
+				else
+					ln -s /opt/remi/php$ver/root/var /opt/remi/php$ver/root/usr/
+				fi
+				if [ ! -d /opt/remi/php$ver/root/etc ];then
+					ln -s /etc/opt/remi/php$ver /opt/remi/php$ver/root/etc
+					ln -s /etc/opt/remi/php$ver /opt/remi/php$ver/root/usr/etc
+				else
+					ln -s /opt/remi/php$ver/root/etc/php-fpm.d /opt/remi/php$ver/root/usr/etc/
+				fi
 				/opt/nDeploy/scripts/update_backend.py PHP PHP$ver /opt/remi/php$ver/root
 				systemctl stop ndeploy_backends
 				systemctl start ndeploy_backends
@@ -139,5 +167,35 @@ else
 		else
 			echo "ERROR: Unknown OS Version detected. Aborting Install "
 		fi
+	}
+
+auto_setup(){
+	if [ $(uname -r|grep lve) ];then
+		setup_lve_patched_php
+	elif [ -f /etc/cpanel/ea4/is_ea4 ];then
+		setup_ea4_php
+	else
+		setup_remi_php
 	fi
+}
+
+#End Function defs
+
+if [ ! -z $1 ];then
+	case $1 in
+		cloudlinux)
+			setup_lve_patched_php
+			;;
+		ea4)
+			setup_ea4_php
+			;;
+		remi)
+			setup_remi_php
+			;;
+		*)
+			auto_setup
+			;;
+	esac
+else
+	auto_setup
 fi
