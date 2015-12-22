c721c15051aec389a1de2b09c298a4b47ef6dabd
Anoop P Alias <anoop.alias@piserve.com>
2015-12-22 21:26:33 +0530

    unison rpmbuild script

---
+++ rpm_buildtree/nDeploy-unison_build_script.centos7.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+UNISON_VERSION="2.48.3"
+UNISON_RPM_ITER="1.el7"
+
+yum install ocaml
+mkdir nDeploy-unison-pkg-centos7
+rm -rf unison-*
+rm -f nDeploy-unison-pkg-centos7/usr/bin/unison*
+wget http://www.seas.upenn.edu/~bcpierce/unison//download/releases/stable/unison-${UNISON_VERSION}.tar.gz
+tar -xvzf unison-${UNISON_VERSION}.tar.gz
+cd unison-${UNISON_VERSION}
+make UISTYLE=text NATIVE=true STATIC=true
+mkdir -p ../nDeploy-unison-pkg-centos7/usr/bin
+rsync -av unison ../nDeploy-unison-pkg-centos7/usr/bin/
+
+fpm -s dir -t rpm -C ../nDeploy-unison-pkg-centos7 --vendor "PiServe Technologies" --version ${UNISON_VERSION} --iteration ${UNISON_RPM_ITER} -a $(arch) -m info@piserve.com -e --description "nDeploy custom unison package" --url http://piserve.com --conflicts unison -d ocaml --name unison-nDeploy .
