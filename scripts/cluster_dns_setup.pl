7dfc4bed3ad0ad1b6f1864cff1fd224bc83d9cb3
Rajiv <rajivcj12@gmail.com>
2015-12-31 08:36:23 +0530

    Cluster_dns_setup using accesshash and also some minor changes

--- scripts/cluster_dns_setup.pl
+++ scripts/cluster_dns_setup.pl
@@ -1,17 +1,21 @@
 #!/usr/local/cpanel/3rdparty/bin/perl
 
-#Author: Rajiv C.J
+#Author: Rajiv C.J.
 #Purpose: Adding and removing A records for domains using cPanel API
 use strict;
 use LWP::UserAgent;
 use LWP::Protocol::https;
-use MIME::Base64;
 use Cpanel::JSON::XS qw(encode_json decode_json);
-use Data::Dumper;
-
-
-my $user = "root";
-my $pass = "n?m}9(b4^gB3>1.I";
+my $hash;
+ if ( -e "/root/.accesshash")
+ {
+  $hash = `cat /root/.accesshash`;
+  $hash =~ s/\n//g;
+ }
+ else
+ {
+   print "Unable to locate .accesshash.Please generate one from WHM"
+ }
 #This script accepts 3 arguments ( add/del,domain name,ip)
 my $count_args = $#ARGV + 1;
 my $task = $ARGV[0];
@@ -26,7 +30,7 @@ my $ip = $ARGV[2];
          }
         else
         {
-         print "invalid num of args";
+         print "Invalid num of args\n";
         }
  }
    elsif ( $task eq "del" )
@@ -37,21 +41,20 @@ my $ip = $ARGV[2];
          }
         else
         {
-         print "invalid num of args";
+         print "Invalid num of args\n";
         }
-    print "Del branch";
    }
 
 
 else
 {
- print "Invalid task provided";
+ print "Invalid task provided\n";
 }
 
 sub adddns
 {
- my $auth = "Basic " . MIME::Base64::encode( $user . ":" . $pass );
- my $ua = LWP::UserAgent->new(
+  my $auth = "WHM root:" . $hash;
+  my $ua = LWP::UserAgent->new(
     ssl_opts   => { verify_hostname => 0, SSL_verify_mode => 'SSL_VERIFY_NONE', SSL_use_cert => 0 },
  );
  my $request = HTTP::Request->new( GET => "https://127.0.0.1:2087/json-api/addzonerecord?api.version=1&domain=$domain&name=$domain.&type=A&address=$ip" );
@@ -66,13 +69,13 @@ sub adddns
   }
   else
   {
-   print $result->{metadata}{reason};
+   print $result->{metadata}{reason},"\n";
   }
 }
 
 sub deldns
 {
- my $auth = "Basic " . MIME::Base64::encode( $user . ":" . $pass );
+ my $auth = "WHM root:" . $hash;
  my $ua = LWP::UserAgent->new(
     ssl_opts   => { verify_hostname => 0, SSL_verify_mode => 'SSL_VERIFY_NONE', SSL_use_cert => 0 },
  );
@@ -83,26 +86,36 @@ sub deldns
  $result = decode_json ($result);
  my $address;
  my $line;
+ my $found;
   foreach my $record ( @{ $result->{result}[0]{record} } )
   {
    $address = $record->{address};
    if ($address =~ $ip)
    {
     $line = $record->{Line};
+    $found +=1;
    }
   }
- $request = HTTP::Request->new( POST => "https://127.0.0.1:2087/json-api/removezonerecord?api.version=1&zone=$domain&line=$line");
- $request->header( Authorization => $auth );
- $response = $ua->request($request);
- $result = $response->content;
- $result = decode_json ($result);
- my $status = $result->{metadata}{result};
-  if ( $status )
+  if ($found)
   {
-   print "Successfully removed the record\n";
+    $request = HTTP::Request->new( POST => "https://127.0.0.1:2087/json-api/removezonerecord?api.version=1&zone=$domain&line=$line");
+    $request->header( Authorization => $auth );
+    $response = $ua->request($request);
+    $result = $response->content;
+    $result = decode_json ($result);
+    my $status = $result->{metadata}{result};
+     if ( $status )
+     {
+      print "Successfully removed the record\n";
+     }
+     else
+    {
+     print $result->{metadata}{reason},"\n";
+    }
   }
   else
   {
-   print $result->{metadata}{reason};
+    print "Record not found\n";
+    exit;
   }
 }
