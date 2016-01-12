5a916322d34b3225eaa9c4d1668fe5bee55a45d5
Anoop P Alias <anoop.alias@piserve.com>
2016-01-12 11:14:47 +0530

    Modified hooks for better dealing with php-fpm pool on account deletion/modify

--- scripts/accountremove_hook_pre.py
+++ scripts/accountremove_hook_pre.py
@@ -21,49 +21,6 @@ backend_config_file = installation_path+"/conf/backends.yaml"
 nginx_dir = "/etc/nginx/sites-enabled/"
 
 
-# Function defs
-def remove_php_fpm_pool(user_name):
-    """Remove the php-fpm pools of deleted accounts"""
-    backend_data_yaml = open(backend_config_file, 'r')
-    backend_data_yaml_parsed = yaml.safe_load(backend_data_yaml)
-    backend_data_yaml.close()
-    if "PHP" in backend_data_yaml_parsed:
-        php_backends_dict = backend_data_yaml_parsed["PHP"]
-        for php_path in list(php_backends_dict.values()):
-            phppool_file = installation_path + "/php-fpm.d/" + user_name + ".conf"
-            php_fpm_config = installation_path+"/conf/php-fpm.conf"
-            if os.path.isfile(php_path+"/sbin/php-fpm"):
-                php_fpm_bin = php_path+"/sbin/php-fpm"
-            else:
-                php_fpm_bin = php_path+"/usr/sbin/php-fpm"
-            if os.path.isfile(phppool_file):
-                os.remove(phppool_file)
-            if os.path.isfile(php_path + "/var/run/php-fpm.pid"):
-                with open(php_path + "/var/run/php-fpm.pid") as f:
-                    mypid = f.read()
-                f.close()
-                try:
-                    os.kill(int(mypid), signal.SIGUSR2)
-                except OSError:
-                    subprocess.call(php_fpm_bin+" --prefix "+php_path+" --fpm-config "+php_fpm_config, shell=True)
-                time.sleep(3)
-                try:
-                    with open(php_path + "/var/run/php-fpm.pid") as f:
-                        newpid = f.read()
-                    f.close()
-                except IOError:
-                    subprocess.call(php_fpm_bin+" --prefix "+php_path+" --fpm-config "+php_fpm_config, shell=True)
-                try:
-                    os.kill(int(newpid), 0)
-                except OSError:
-                    subprocess.call(php_fpm_bin+" --prefix "+php_path+" --fpm-config "+php_fpm_config, shell=True)
-                else:
-                    return True
-            else:
-                subprocess.call(php_fpm_bin+" --prefix "+php_path+" --fpm-config "+php_fpm_config, shell=True)
-    return
-
-
 cpjson = json.load(sys.stdin)
 mydict = cpjson["data"]
 cpaneluser = mydict["user"]
@@ -98,6 +55,5 @@ try:
     os.remove(installation_path + "/php-fpm.d/" + cpaneluser + ".conf")
 except OSError:
     pass
-# remove_php_fpm_pool(cpaneluser)
 subprocess.call("/usr/sbin/nginx -s reload", shell=True)
 print(("1 nDeploy:remove:"+cpaneluser))
