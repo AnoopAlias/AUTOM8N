a8bd4b6e423720ecc5097eb42f6e6b907ba4b624
Anoop P Alias <anoop.alias@piserve.com>
2016-01-30 13:10:58 +0530

    remove dep for unison

--- rpm_buildtree/nDeploy-unison_build_script.centos7.sh
+++ rpm_buildtree/nDeploy-unison_build_script.centos7.sh
@@ -2,7 +2,7 @@
 UNISON_VERSION="2.48.3"
 UNISON_RPM_ITER="2.el7"
 
-yum install ocaml
+yum install ocaml ocaml-ssl-devel
 mkdir nDeploy-unison-pkg-centos7
 rm -rf unison-*
 rm -f nDeploy-unison-pkg-centos7/usr/bin/unison*
@@ -14,5 +14,5 @@ make UISTYLE=text NATIVE=true STATIC=true
 mkdir -p ../nDeploy-unison-pkg-centos7/usr/bin
 rsync -av unison ../nDeploy-unison-pkg-centos7/usr/bin/
 cd ../nDeploy-unison-pkg-centos7
-fpm -s dir -t rpm -C ../nDeploy-unison-pkg-centos7 --vendor "PiServe Technologies" --version ${UNISON_VERSION} --iteration ${UNISON_RPM_ITER} -a $(arch) -m info@piserve.com -e --description "nDeploy custom unison package" --url http://piserve.com --conflicts unison -d ocaml --name unison-nDeploy .
+fpm -s dir -t rpm -C ../nDeploy-unison-pkg-centos7 --vendor "PiServe Technologies" --version ${UNISON_VERSION} --iteration ${UNISON_RPM_ITER} -a $(arch) -m info@piserve.com -e --description "nDeploy custom unison package" --url http://piserve.com --conflicts unison --name unison-nDeploy .
 rsync -av *.rpm root@rpm.piserve.com:/home/rpmrepo/public_html/CentOS/7/x86_64/
--- rpm_buildtree/nDeploy-unison_build_script.sh
+++ rpm_buildtree/nDeploy-unison_build_script.sh
@@ -2,7 +2,7 @@
 UNISON_VERSION="2.48.3"
 UNISON_RPM_ITER="2.el6"
 
-yum install ocaml
+yum install ocaml ocaml-ssl-devel
 mkdir nDeploy-unison-pkg
 rm -rf unison-*
 rm -f nDeploy-unison-pkg/usr/bin/unison*
@@ -14,5 +14,5 @@ make UISTYLE=text NATIVE=true STATIC=true
 mkdir -p ../nDeploy-unison-pkg/usr/bin
 rsync -av unison ../nDeploy-unison-pkg/usr/bin/
 cd ../nDeploy-unison-pkg
-fpm -s dir -t rpm -C ../nDeploy-unison-pkg --vendor "PiServe Technologies" --version ${UNISON_VERSION} --iteration ${UNISON_RPM_ITER} -a $(arch) -m info@piserve.com -e --description "nDeploy custom unison package" --url http://piserve.com --conflicts unison -d ocaml --name unison-nDeploy .
+fpm -s dir -t rpm -C ../nDeploy-unison-pkg --vendor "PiServe Technologies" --version ${UNISON_VERSION} --iteration ${UNISON_RPM_ITER} -a $(arch) -m info@piserve.com -e --description "nDeploy custom unison package" --url http://piserve.com --conflicts unison --name unison-nDeploy .
 rsync -av *.rpm root@rpm.piserve.com:/home/rpmrepo/public_html/CentOS/6/x86_64/
