e38867b495eeac2cff7f57339f736077afc24421
Anoop P Alias <anoop.alias@piserve.com>
2016-02-17 15:01:34 +0530

    Add GPG signing to packages

--- rpm_buildtree/nDeploy-unison_build_script.sh
+++ rpm_buildtree/nDeploy-unison_build_script.sh
@@ -14,5 +14,5 @@ make UISTYLE=text NATIVE=true STATIC=true
 mkdir -p ../nDeploy-unison-pkg/usr/bin
 rsync -av unison ../nDeploy-unison-pkg/usr/bin/
 cd ../nDeploy-unison-pkg
-fpm -s dir -t rpm -C ../nDeploy-unison-pkg --vendor "PiServe Technologies" --version ${UNISON_VERSION} --iteration ${UNISON_RPM_ITER} -a $(arch) -m info@piserve.com -e --description "nDeploy custom unison package" --url http://piserve.com --conflicts unison --name unison-nDeploy .
+fpm -s dir -t rpm -C ../nDeploy-unison-pkg --vendor "PiServe Technologies" --version ${UNISON_VERSION} --iteration ${UNISON_RPM_ITER} --rpm-sign -a $(arch) -m info@piserve.com -e --description "nDeploy custom unison package" --url http://piserve.com --conflicts unison --name unison-nDeploy .
 rsync -av *.rpm root@rpm.piserve.com:/home/rpmrepo/public_html/CentOS/6/x86_64/
--- rpm_buildtree/nDeploy_build_script.sh
+++ rpm_buildtree/nDeploy_build_script.sh
@@ -2,7 +2,7 @@
 #Author: Anoop P Alias
 
 NDEPLOY_VERSION="2.0"
-RPM_ITERATION="37"
+RPM_ITERATION="38"
 
 rm -f nDeploy-pkg/nDeploy-* nDeploy-pkg-centos7/nDeploy-*
 rsync -av ../scripts/ nDeploy-pkg/opt/nDeploy/scripts/
@@ -15,12 +15,12 @@ rsync -av ../nDeploy_cp/ nDeploy-pkg-centos7/opt/nDeploy/nDeploy_cp/
 
 cd nDeploy-pkg
 mkdir opt/nDeploy/lock
-fpm -s dir -t rpm -C ../nDeploy-pkg --vendor "PiServe Technologies" --version ${NDEPLOY_VERSION} --iteration ${RPM_ITERATION}.el6 -d python-inotify -d nginx-nDeploy -d python-argparse -d PyYAML -d python-lxml -a noarch -m info@piserve.com -e --description "nDeploy cPanel plugin" --url http://piserve.com --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
+fpm -s dir -t rpm -C ../nDeploy-pkg --vendor "PiServe Technologies" --version ${NDEPLOY_VERSION} --iteration ${RPM_ITERATION}.el6 --rpm-sign -d python-inotify -d nginx-nDeploy -d python-argparse -d PyYAML -d python-lxml -a noarch -m info@piserve.com -e --description "nDeploy cPanel plugin" --url http://piserve.com --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
 rsync -av nDeploy-* root@rpm.piserve.com:/home/rpmrepo/public_html/CentOS/6/x86_64/
 
 cd ..
 cd nDeploy-pkg-centos7
 mkdir opt/nDeploy/lock
-fpm -s dir -t rpm -C ../nDeploy-pkg-centos7 --vendor "PiServe Technologies" --version ${NDEPLOY_VERSION} --iteration ${RPM_ITERATION}.el7 -d python-inotify -d nginx-nDeploy -d python-argparse -d PyYAML -d python-lxml -a noarch -m info@piserve.com -e --description "nDeploy cPanel plugin" --url http://piserve.com --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
+fpm -s dir -t rpm -C ../nDeploy-pkg-centos7 --vendor "PiServe Technologies" --version ${NDEPLOY_VERSION} --iteration ${RPM_ITERATION}.el7 --rpm-sign -d python-inotify -d nginx-nDeploy -d python-argparse -d PyYAML -d python-lxml -a noarch -m info@piserve.com -e --description "nDeploy cPanel plugin" --url http://piserve.com --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
 rsync -av nDeploy-* root@rpm.piserve.com:/home/rpmrepo/public_html/CentOS/7/x86_64/
 cd ..
