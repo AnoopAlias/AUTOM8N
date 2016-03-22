ffbba6ec7ec9eb1853a0cd1a1bde3137a80e9393
Anoop P Alias <anoop.alias@piserve.com>
2016-03-22 07:13:59 +0530

    Fixes commit #8704fa4

--- rpm_buildtree/nDeploy_build_script.sh
+++ rpm_buildtree/nDeploy_build_script.sh
@@ -2,7 +2,7 @@
 #Author: Anoop P Alias
 
 NDEPLOY_VERSION="2.0"
-RPM_ITERATION="46"
+RPM_ITERATION="47"
 
 rm -f nDeploy-pkg/nDeploy-* nDeploy-pkg-centos7/nDeploy-*
 rsync -av ../scripts/ nDeploy-pkg/opt/nDeploy/scripts/
