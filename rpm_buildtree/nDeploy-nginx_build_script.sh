a41f99315b8226d860d27264d60a00bed8a2a829
Anoop P Alias <anoopalias01@gmail.com>
2016-04-27 13:26:42 +0530

    Update NAXSI version for dynamic module support and added http2 instead of spdy in conf

--- rpm_buildtree/nDeploy-nginx_build_script.sh
+++ rpm_buildtree/nDeploy-nginx_build_script.sh
@@ -8,7 +8,7 @@ NPS_VERSION="1.11.33.0"
 MY_RUBY_VERSION="2.3.0"
 PASSENGER_VERSION="5.0.27"
 CACHE_PURGE_VERSION="2.3"
-NAXSI_VERSION="0.54"
+NAXSI_VERSION="0.55rc1"
 PS_NGX_EXTRA_FLAGS="--with-cc=/opt/rh/devtoolset-2/root/usr/bin/gcc"
 
 rm -f nginx-pkg-64/nginx-nDeploy*
@@ -33,6 +33,7 @@ wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 tar -xvzf nginx-${NGINX_VERSION}.tar.gz
 cd nginx-${NGINX_VERSION}/
 
+
 wget https://github.com/nbs-system/naxsi/archive/${NAXSI_VERSION}.tar.gz
 tar -xvzf ${NAXSI_VERSION}.tar.gz
 sed -i 's/hash_init.bucket_size = 512/hash_init.bucket_size = 2048/' naxsi-${NAXSI_VERSION}/naxsi_src/naxsi_utils.c
@@ -48,7 +49,7 @@ wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
 tar -xzvf ${NPS_VERSION}.tar.gz
 cd ..
 
-./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib64/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error_log --http-log-path=/var/log/nginx/access_log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nobody --group=nobody --add-dynamic-module=naxsi-${NAXSI_VERSION}/naxsi_src --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --with-file-aio --with-threads --with-stream --with-stream_ssl_module --with-http_slice_module --with-ipv6 --with-http_v2_module --add-dynamic-module=ngx_pagespeed-release-${NPS_VERSION}-beta ${PS_NGX_EXTRA_FLAGS} --add-module=/usr/local/rvm/gems/ruby-${MY_RUBY_VERSION}/gems/passenger-${PASSENGER_VERSION}/src/nginx_module --add-module=ngx_cache_purge-${CACHE_PURGE_VERSION} --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic' --with-ld-opt=-Wl,-E
+./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/etc/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error_log --http-log-path=/var/log/nginx/access_log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nobody --group=nobody --add-dynamic-module=naxsi-${NAXSI_VERSION}/naxsi_src --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --with-file-aio --with-threads --with-stream --with-stream_ssl_module --with-http_slice_module --with-ipv6 --with-http_v2_module --add-dynamic-module=ngx_pagespeed-release-${NPS_VERSION}-beta ${PS_NGX_EXTRA_FLAGS} --add-module=/usr/local/rvm/gems/ruby-${MY_RUBY_VERSION}/gems/passenger-${PASSENGER_VERSION}/src/nginx_module --add-module=ngx_cache_purge-${CACHE_PURGE_VERSION} --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic' --with-ld-opt=-Wl,-E
 make DESTDIR=./tempo install
 
 git clone https://github.com/nbs-system/naxsi-rules.git
@@ -58,7 +59,7 @@ rm -rf ../nginx-pkg-64/usr/nginx/nxapi
 rsync -av naxsi-${NAXSI_VERSION}/nxapi ../nginx-pkg-64/usr/nginx/
 rsync -av ../nxapi.json ../nginx-pkg-64/usr/nginx/nxapi/
 rsync -av tempo/usr/sbin ../nginx-pkg-64/usr/
-rsync -av tempo/usr/lib64 ../nginx-pkg-64/usr/
+rsync -av tempo/etc/nginx/modules ../nginx-pkg-64/etc/nginx/
 
 sed -i "s/RUBY_VERSION/$MY_RUBY_VERSION/g" ../nginx-pkg-64/etc/nginx/conf.d/passenger.conf
 sed -i "s/PASSENGER_VERSION/$PASSENGER_VERSION/g" ../nginx-pkg-64/etc/nginx/conf.d/passenger.conf
