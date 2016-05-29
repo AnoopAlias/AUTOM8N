7a33c1102d829af9f4e1959b2d37c62049704c20
Anoop P Alias <anoopalias01@gmail.com>
2016-05-29 18:06:24 +0530

    Version Bump

--- rpm_buildtree/nDeploy-cluster-slave_build_script.sh
+++ rpm_buildtree/nDeploy-cluster-slave_build_script.sh
@@ -2,15 +2,15 @@
 #Author: Anoop P Alias
 
 NDEPLOY_VERSION="2.0"
-RPM_ITERATION="14"
+RPM_ITERATION="15"
 
 rm -f nDeploy-cluster-slave-pkg-centos7/nDeploy-*
-rsync -av ../scripts/postfix_backupmx_update.sh ../scripts/cluster_slave_setup_backends.py ../scripts/easy_php_setup.sh ../scripts/init_backends.py ../scripts/update_backend.py nDeploy-cluster-slave-pkg-centos7/opt/nDeploy/scripts/
+rsync -av ../scripts/ddos_mitigate.sh ../scripts/postfix_backupmx_setup.sh ../scripts/postfix_backupmx_update.sh ../scripts/cluster_slave_setup_backends.py ../scripts/easy_php_setup.sh ../scripts/init_backends.py ../scripts/update_backend.py nDeploy-cluster-slave-pkg-centos7/opt/nDeploy/scripts/
 rsync -av ../conf/php-fpm.conf ../conf/php-fpm.pool.tmpl nDeploy-cluster-slave-pkg-centos7/opt/nDeploy/conf/
 rsync -av nDeploy-pkg-centos7/usr/lib/systemd/system/ndeploy_backends.service  nDeploy-cluster-slave-pkg-centos7/usr/lib/systemd/system/ndeploy_backends.service
 
 rm -f nDeploy-cluster-slave-pkg/nDeploy-*
-rsync -av ../scripts/postfix_backupmx_update.sh ../scripts/cluster_slave_setup_backends.py ../scripts/easy_php_setup.sh ../scripts/init_backends.py ../scripts/update_backend.py nDeploy-cluster-slave-pkg/opt/nDeploy/scripts/
+rsync -av ../scripts/ddos_mitigate.sh ../scripts/postfix_backupmx_setup.sh ../scripts/postfix_backupmx_update.sh ../scripts/cluster_slave_setup_backends.py ../scripts/easy_php_setup.sh ../scripts/init_backends.py ../scripts/update_backend.py nDeploy-cluster-slave-pkg/opt/nDeploy/scripts/
 rsync -av ../conf/php-fpm.conf ../conf/php-fpm.pool.tmpl nDeploy-cluster-slave-pkg/opt/nDeploy/conf/
 rsync -av nDeploy-pkg/etc/rc.d/init.d/ndeploy_backends nDeploy-cluster-slave-pkg/etc/rc.d/init.d/ndeploy_backends
 
