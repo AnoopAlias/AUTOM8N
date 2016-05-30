1f3646a206e9a35bafb5c07c25cf94370c353837
Anoop P Alias <anoopalias01@gmail.com>
2016-05-30 07:13:12 +0530

    Minor ansible playbook fixes

--- scripts/postfix_backupmx_update.sh
+++ scripts/postfix_backupmx_update.sh
@@ -2,5 +2,6 @@
 #Author : Anoop P Alias
 #csync2 action script to configure postfix relaydomains in cPanel
 sed 's/$/ OK/' /etc/localdomains > /etc/postfix/relaydomains
+chmod 644 /etc/postfix/relaydomains
 postmap /etc/postfix/relaydomains
 postfix reload
