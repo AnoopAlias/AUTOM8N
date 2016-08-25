44075bd98adec4d9b67e7e0234e7b9b5699c0c2e
Anoop P Alias <anoopalias01@gmail.com>
2016-08-25 11:04:40 +0530

    add libbrotli rpm build script

---
+++ rpm_buildtree/nDeploy-libbrotli_build_script.centos7.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Author: Anoop P Alias
+
+##Vars
+BROTLI_VERSION="1.0"
+BROTLI_RPM_ITER="1.el7"
+
+rm -rf /opt/libbrotli
+git clone https://github.com/bagder/libbrotli
+cd libbrotli
+./autogen.sh
+./configure --prefix=/usr --libdir=/usr/lib64
+make DESTDIR=/opt/libbrotli install
+cd /opt/libbrotli
+
+fpm -s dir -t rpm -C ../libbrotli --vendor "Anoop P Alias" --version ${BROTLI_VERSION} --iteration ${BROTLI_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com -e --description "nDeploy custom libbrotli package" --url https://github.com/bagder/libbrotli --conflicts libbrotli --name libbrotli-nDeploy .
+rsync -av libbrotli-nDeploy-* root@rpm.piserve.com:/home/gnusys/public_html/CentOS/7/x86_64/
