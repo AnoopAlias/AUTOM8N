ca79612c6d9624df298095e22079ffcf891123b2
Anoop P Alias <anoopalias01@gmail.com>
2016-08-25 15:30:01 +0530

    nginx-nDeploy added support for brotli and GeoIP modules

--- rpm_buildtree/nDeploy-libbrotli_build_script.sh
+++ rpm_buildtree/nDeploy-libbrotli_build_script.sh
@@ -3,7 +3,7 @@
 
 ##Vars
 BROTLI_VERSION="1.0"
-BROTLI_RPM_ITER="1.el7"
+BROTLI_RPM_ITER="1.el6"
 
 rm -rf /opt/libbrotli
 git clone https://github.com/bagder/libbrotli
@@ -14,4 +14,4 @@ make DESTDIR=/opt/libbrotli install
 cd /opt/libbrotli
 
 fpm -s dir -t rpm -C ../libbrotli --vendor "Anoop P Alias" --version ${BROTLI_VERSION} --iteration ${BROTLI_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com -e --description "nDeploy custom libbrotli package" --url https://github.com/bagder/libbrotli --conflicts libbrotli --name libbrotli-nDeploy .
-rsync -av libbrotli-nDeploy-* root@rpm.piserve.com:/home/gnusys/public_html/CentOS/7/x86_64/
+rsync -av libbrotli-nDeploy-* root@rpm.piserve.com:/home/gnusys/public_html/CentOS/6/x86_64/
--- rpm_buildtree/nDeploy-nginx_build_script.sh
+++ rpm_buildtree/nDeploy-nginx_build_script.sh
@@ -3,7 +3,7 @@
 
 ##Vars
 NGINX_VERSION="1.11.3"
-NGINX_RPM_ITER="1.el6"
+NGINX_RPM_ITER="2.el6"
 NPS_VERSION="1.11.33.2"
 MY_RUBY_VERSION="2.3.0"
 PASSENGER_VERSION="5.0.30"
@@ -53,7 +53,9 @@ cd ..
 wget https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
 tar -zxf openssl-${OPENSSL_VERSION}.tar.gz
 
-./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/etc/nginx/modules --with-openssl=./openssl-${OPENSSL_VERSION} --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error_log --http-log-path=/var/log/nginx/access_log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nobody --group=nobody --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --add-dynamic-module=naxsi-${NAXSI_VERSION}/naxsi_src --with-file-aio --with-threads --with-stream --with-stream_ssl_module --with-http_slice_module --with-ipv6 --with-http_v2_module --add-dynamic-module=ngx_pagespeed-release-${NPS_VERSION}-beta ${PS_NGX_EXTRA_FLAGS} --add-dynamic-module=/usr/local/rvm/gems/ruby-${MY_RUBY_VERSION}/gems/passenger-${PASSENGER_VERSION}/src/nginx_module --add-module=ngx_cache_purge-${CACHE_PURGE_VERSION} --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic' --with-ld-opt=-Wl,-E
+git clone https://github.com/google/ngx_brotli.git
+
+./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/etc/nginx/modules --with-openssl=./openssl-${OPENSSL_VERSION} --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error_log --http-log-path=/var/log/nginx/access_log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nobody --group=nobody --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --add-dynamic-module=naxsi-${NAXSI_VERSION}/naxsi_src --with-file-aio --with-threads --with-stream --with-stream_ssl_module --with-http_slice_module --with-ipv6 --with-http_v2_module --with-http_geoip_module=dynamic --add-dynamic-module=ngx_pagespeed-release-${NPS_VERSION}-beta ${PS_NGX_EXTRA_FLAGS} --add-dynamic-module=/usr/local/rvm/gems/ruby-${MY_RUBY_VERSION}/gems/passenger-${PASSENGER_VERSION}/src/nginx_module --add-module=ngx_cache_purge-${CACHE_PURGE_VERSION} --add-module=ngx_brotli --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic' --with-ld-opt=-Wl,-E
 make DESTDIR=./tempo install
 
 git clone https://github.com/nbs-system/naxsi-rules.git
@@ -76,5 +78,5 @@ cd ../nginx-pkg-64
 mkdir -p var/cache/nginx/ngx_pagespeed
 mkdir -p var/log/nginx
 mkdir -p var/run
-fpm -s dir -t rpm -C ../nginx-pkg-64 --vendor "PiServe Technologies" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m info@piserve.com -e --description "nDeploy custom nginx package" --url http://piserve.com --conflicts nginx -d zlib -d openssl -d pcre -d libcurl -d memcached --after-install ../after_nginx_install --before-remove ../after_nginx_uninstall --name nginx-nDeploy .
+fpm -s dir -t rpm -C ../nginx-pkg-64 --vendor "PiServe Technologies" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m info@piserve.com -e --description "nDeploy custom nginx package" --url http://piserve.com --conflicts nginx -d GeoIP -d GeoIP-update -d libbrotli-nDeploy -d zlib -d openssl -d pcre -d libcurl -d memcached --after-install ../after_nginx_install --before-remove ../after_nginx_uninstall --name nginx-nDeploy .
 rsync -av nginx-nDeploy-* root@rpm.piserve.com:/home/gnusys/public_html/CentOS/6/x86_64/
