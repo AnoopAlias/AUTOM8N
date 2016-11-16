cde6ef2e892b4dde53fbf0a90102b810ee6f53e2
Anoop P Alias <anoopalias01@gmail.com>
2016-11-16 19:41:47 +0530

    bug fix in subdir proxy template

--- rpm_buildtree/nDeploy_build_script.sh
+++ rpm_buildtree/nDeploy_build_script.sh
@@ -2,7 +2,7 @@
 #Author: Anoop P Alias
 
 NDEPLOY_VERSION="4.0"
-RPM_ITERATION="12"
+RPM_ITERATION="13"
 
 rm -f nDeploy-pkg/nDeploy-* nDeploy-pkg-centos7/nDeploy-*
 rsync -av ../scripts/ nDeploy-pkg/opt/nDeploy/scripts/
