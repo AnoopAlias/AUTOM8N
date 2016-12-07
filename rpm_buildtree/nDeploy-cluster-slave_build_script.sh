0ae4606db56a6681b883102d460c5ad1086debae
Anoop P Alias <anoopalias01@gmail.com>
2016-12-07 11:06:59 +0530

    Fixes #128

--- rpm_buildtree/nDeploy-cluster-slave_build_script.sh
+++ rpm_buildtree/nDeploy-cluster-slave_build_script.sh
@@ -5,12 +5,12 @@ NDEPLOY_VERSION="4.0"
 RPM_ITERATION="3"
 
 rm -f nDeploy-cluster-slave-pkg-centos7/nDeploy-*
-rsync -av ../scripts/postfix_backupmx_setup.sh ../scripts/postfix_backupmx_update.sh ../scripts/easy_php_setup.sh ../scripts/init_backends.py ../scripts/update_backend.py nDeploy-cluster-slave-pkg-centos7/opt/nDeploy/scripts/
+rsync -av ../scripts/generate_default_vhost_config.py ../scripts/postfix_backupmx_setup.sh ../scripts/postfix_backupmx_update.sh ../scripts/easy_php_setup.sh ../scripts/init_backends.py ../scripts/update_backend.py nDeploy-cluster-slave-pkg-centos7/opt/nDeploy/scripts/
 rsync -av ../conf/php-fpm.conf nDeploy-cluster-slave-pkg-centos7/opt/nDeploy/conf/
 rsync -av nDeploy-pkg-centos7/usr/lib/systemd/system/ndeploy_backends.service  nDeploy-cluster-slave-pkg-centos7/usr/lib/systemd/system/ndeploy_backends.service
 
 rm -f nDeploy-cluster-slave-pkg/nDeploy-*
-rsync -av ../scripts/postfix_backupmx_setup.sh ../scripts/postfix_backupmx_update.sh ../scripts/easy_php_setup.sh ../scripts/init_backends.py ../scripts/update_backend.py nDeploy-cluster-slave-pkg/opt/nDeploy/scripts/
+rsync -av ../scripts/generate_default_vhost_config.py ../scripts/postfix_backupmx_setup.sh ../scripts/postfix_backupmx_update.sh ../scripts/easy_php_setup.sh ../scripts/init_backends.py ../scripts/update_backend.py nDeploy-cluster-slave-pkg/opt/nDeploy/scripts/
 rsync -av ../conf/php-fpm.conf nDeploy-cluster-slave-pkg/opt/nDeploy/conf/
 rsync -av nDeploy-pkg/etc/rc.d/init.d/ndeploy_backends nDeploy-cluster-slave-pkg/etc/rc.d/init.d/ndeploy_backends
 
