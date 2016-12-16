4e0811f0023c107acc4ddb6291757b532b6253d1
Anoop P Alias <anoopalias01@gmail.com>
2016-12-16 16:22:39 +0530

    add echo module

--- rpm_buildtree/nDeploy-nginx_build_script.sh
+++ rpm_buildtree/nDeploy-nginx_build_script.sh
@@ -20,6 +20,7 @@ REDIS2_NGINX_MODULE="0.13"
 SET_MISC_NGINX_MODULE="0.31"
 REDIS_NGINX_MODULE="0.3.8"
 HEADERS_MORE_NGINX_MODULE="0.32"
+ECHO_NGINX_MODULE="0.60"
 
 
 
@@ -32,7 +33,7 @@ mkdir -p nginx-pkg/var/cache/nginx/ngx_pagespeed
 mkdir -p nginx-pkg/var/log/nginx
 mkdir -p nginx-pkg/var/run
 
-for module in brotli geoip naxsi pagespeed passenger redis redis2 set_misc srcache_filter
+for module in brotli geoip naxsi pagespeed passenger redis redis2 set_misc srcache_filter echo
 do
   mkdir -p nginx-module-${module}-pkg/etc/nginx/{modules,modules.d,conf.auto,conf.d}
   mkdir -p nginx-module-${module}-pkg/usr/nginx/scripts
@@ -90,6 +91,7 @@ wget -O srcache-nginx-module.tgz https://github.com/openresty/srcache-nginx-modu
 wget -O ngx_devel_kit.tgz https://github.com/simpl/ngx_devel_kit/archive/v${NGX_DEVEL_KIT}.tar.gz
 wget -O set-misc-nginx-module.tgz https://github.com/openresty/set-misc-nginx-module/archive/v${SET_MISC_NGINX_MODULE}.tar.gz
 wget -O headers-more-nginx-module.tgz https://github.com/openresty/headers-more-nginx-module/archive/v${HEADERS_MORE_NGINX_MODULE}.tar.gz
+wget -O echo-nginx-module.tgz https://github.com/openresty/echo-nginx-module/archive/v${ECHO_NGINX_MODULE}.tar.gz
 wget http://people.freebsd.org/~osa/ngx_http_redis-${REDIS_NGINX_MODULE}.tar.gz
 #wget -O redis2-nginx-module.tgz https://github.com/openresty/redis2-nginx-module/archive/v${REDIS2_NGINX_MODULE}.tar.gz
 tar -xvzf ngx_http_redis-${REDIS_NGINX_MODULE}.tar.gz
@@ -97,13 +99,14 @@ tar -xvzf srcache-nginx-module.tgz
 tar -xvzf ngx_devel_kit.tgz
 tar -xvzf set-misc-nginx-module.tgz
 tar -xvzf headers-more-nginx-module.tgz
+tar -xvzf echo-nginx-module.tgz
 #tar -xvzf redis2-nginx-module.tgz
 git clone https://github.com/openresty/redis2-nginx-module.git
 
 if [ ${OSVERSION} -eq 6 ];then
-./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/etc/nginx/modules --with-openssl=./openssl-${OPENSSL_VERSION} --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error_log --http-log-path=/var/log/nginx/access_log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nobody --group=nobody --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --add-dynamic-module=naxsi-${NAXSI_VERSION}/naxsi_src --with-file-aio --with-threads --with-stream --with-stream_ssl_module --with-http_slice_module  --with-compat --with-http_v2_module --with-http_geoip_module=dynamic --add-dynamic-module=ngx_pagespeed-release-${NPS_VERSION}-beta ${PS_NGX_EXTRA_FLAGS} --add-dynamic-module=/usr/local/rvm/gems/ruby-${MY_RUBY_VERSION}/gems/passenger-${PASSENGER_VERSION}/src/nginx_module --add-dynamic-module=ngx_brotli --add-dynamic-module=headers-more-nginx-module-${HEADERS_MORE_NGINX_MODULE} --add-dynamic-module=ngx_http_redis-${REDIS_NGINX_MODULE} --add-dynamic-module=redis2-nginx-module --add-dynamic-module=srcache-nginx-module-${SRCACHE_NGINX_MODULE} --add-dynamic-module=ngx_devel_kit-${NGX_DEVEL_KIT} --add-dynamic-module=set-misc-nginx-module-${SET_MISC_NGINX_MODULE} --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic' --with-ld-opt=-Wl,-E
+./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/etc/nginx/modules --with-openssl=./openssl-${OPENSSL_VERSION} --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error_log --http-log-path=/var/log/nginx/access_log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nobody --group=nobody --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --add-dynamic-module=naxsi-${NAXSI_VERSION}/naxsi_src --with-file-aio --with-threads --with-stream --with-stream_ssl_module --with-http_slice_module  --with-compat --with-http_v2_module --with-http_geoip_module=dynamic --add-dynamic-module=ngx_pagespeed-release-${NPS_VERSION}-beta ${PS_NGX_EXTRA_FLAGS} --add-dynamic-module=/usr/local/rvm/gems/ruby-${MY_RUBY_VERSION}/gems/passenger-${PASSENGER_VERSION}/src/nginx_module --add-dynamic-module=ngx_brotli --add-dynamic-module=echo-nginx-module-${ECHO_NGINX_MODULE} --add-dynamic-module=headers-more-nginx-module-${HEADERS_MORE_NGINX_MODULE} --add-dynamic-module=ngx_http_redis-${REDIS_NGINX_MODULE} --add-dynamic-module=redis2-nginx-module --add-dynamic-module=srcache-nginx-module-${SRCACHE_NGINX_MODULE} --add-dynamic-module=ngx_devel_kit-${NGX_DEVEL_KIT} --add-dynamic-module=set-misc-nginx-module-${SET_MISC_NGINX_MODULE} --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic' --with-ld-opt=-Wl,-E
 else
-./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/etc/nginx/modules --with-openssl=./openssl-${OPENSSL_VERSION} --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error_log --http-log-path=/var/log/nginx/access_log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nobody --group=nobody --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --add-dynamic-module=naxsi-${NAXSI_VERSION}/naxsi_src --with-file-aio --with-threads --with-stream --with-stream_ssl_module --with-http_slice_module --with-compat --with-http_v2_module --with-http_geoip_module=dynamic --add-dynamic-module=ngx_pagespeed-release-${NPS_VERSION}-beta --add-dynamic-module=/usr/local/rvm/gems/ruby-${MY_RUBY_VERSION}/gems/passenger-${PASSENGER_VERSION}/src/nginx_module --add-dynamic-module=ngx_brotli --add-dynamic-module=headers-more-nginx-module-${HEADERS_MORE_NGINX_MODULE} --add-dynamic-module=ngx_http_redis-${REDIS_NGINX_MODULE} --add-dynamic-module=redis2-nginx-module --add-dynamic-module=srcache-nginx-module-${SRCACHE_NGINX_MODULE} --add-dynamic-module=ngx_devel_kit-${NGX_DEVEL_KIT} --add-dynamic-module=set-misc-nginx-module-${SET_MISC_NGINX_MODULE} --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic' --with-ld-opt=-Wl,-E
+./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/etc/nginx/modules --with-openssl=./openssl-${OPENSSL_VERSION} --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error_log --http-log-path=/var/log/nginx/access_log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nobody --group=nobody --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --add-dynamic-module=naxsi-${NAXSI_VERSION}/naxsi_src --with-file-aio --with-threads --with-stream --with-stream_ssl_module --with-http_slice_module --with-compat --with-http_v2_module --with-http_geoip_module=dynamic --add-dynamic-module=ngx_pagespeed-release-${NPS_VERSION}-beta --add-dynamic-module=/usr/local/rvm/gems/ruby-${MY_RUBY_VERSION}/gems/passenger-${PASSENGER_VERSION}/src/nginx_module --add-dynamic-module=ngx_brotli --add-dynamic-module=echo-nginx-module-${ECHO_NGINX_MODULE} --add-dynamic-module=headers-more-nginx-module-${HEADERS_MORE_NGINX_MODULE} --add-dynamic-module=ngx_http_redis-${REDIS_NGINX_MODULE} --add-dynamic-module=redis2-nginx-module --add-dynamic-module=srcache-nginx-module-${SRCACHE_NGINX_MODULE} --add-dynamic-module=ngx_devel_kit-${NGX_DEVEL_KIT} --add-dynamic-module=set-misc-nginx-module-${SET_MISC_NGINX_MODULE} --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic' --with-ld-opt=-Wl,-E
 fi
 make DESTDIR=./tempo install
 
@@ -117,7 +120,7 @@ rsync -a ../nginx-pkg-64-common/etc/nginx/conf.d/pagespeed.conf ../nginx-module-
 rsync -a ../nginx-pkg-64-common/etc/nginx/conf.d/brotli.conf ../nginx-module-brotli-pkg/etc/nginx/conf.d/
 rsync -a ../nginx-pkg-64-common/etc/nginx/conf.d/naxsi_* ../nginx-module-naxsi-pkg/etc/nginx/conf.d/
 rsync -a tempo/usr/sbin ../nginx-pkg/usr/
-for module in brotli geoip naxsi pagespeed passenger redis redis2 set_misc srcache_filter
+for module in brotli geoip naxsi pagespeed passenger redis redis2 set_misc srcache_filter echo
 do
   rsync -a tempo/etc/nginx/modules/ngx_http_${module}* ../nginx-module-${module}-pkg/etc/nginx/modules/
   rsync -a ../nginx-pkg-64-common/etc/nginx/conf.auto/${module}.conf ../nginx-module-${module}-pkg/etc/nginx/conf.auto/
@@ -145,7 +148,7 @@ cd ../nginx-pkg
 fpm -s dir -t rpm -C ../nginx-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com -e --description "nDeploy custom nginx package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx -d zlib -d pcre -d libcurl --after-install ../after_nginx_install --before-remove ../after_nginx_uninstall --name nginx-nDeploy .
 rsync -a nginx-nDeploy-* root@gnusys.net:/usr/share/nginx/html/CentOS/${OSVERSION}/x86_64/
 
-for module in brotli geoip naxsi pagespeed passenger redis redis2 set_misc srcache_filter
+for module in brotli geoip naxsi pagespeed passenger redis redis2 set_misc srcache_filter echo
 do
   cd ../nginx-module-${module}-pkg
   if [ ${module} == "brotli" ];then
