beeda370cb82aa653895a5c095982d7f18b38290
Anoop <anoop@yoga.gnusys.net>
2017-02-16 11:45:56 +0530

    version bump and minor fix in hook

--- rpm_buildtree/nDeploy_build_script.sh
+++ rpm_buildtree/nDeploy_build_script.sh
@@ -2,7 +2,7 @@
 #Author: Anoop P Alias
 
 NDEPLOY_VERSION="4.1"
-RPM_ITERATION="6"
+RPM_ITERATION="7"
 
 rm -f nDeploy-pkg/nDeploy-* nDeploy-pkg-centos7/nDeploy-*
 rsync -av ../scripts/ nDeploy-pkg/opt/nDeploy/scripts/
--- scripts/accountremove_hook_pre.py
+++ scripts/accountremove_hook_pre.py
@@ -2,6 +2,7 @@
 
 
 import sys
+import os
 try:
     import simplejson as json
 except ImportError:
@@ -18,6 +19,14 @@ installation_path = "/opt/nDeploy"  # Absolute Installation Path
 nginx_dir = "/etc/nginx/sites-enabled/"
 
 
+# Define a function to silently remove files
+def silentremove(filename):
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+
+
 cpjson = json.load(sys.stdin)
 mydict = cpjson["data"]
 cpaneluser = mydict["user"]
@@ -25,6 +34,8 @@ cpaneluser = mydict["user"]
 # we copy the associated  userdata file for the user for post hook stage
 with open("/var/cpanel/userdata/" + cpaneluser + "/main.cache", 'r') as userdata:
     userdata_json = json.load(userdata)
+# remove any stale userdata tmp files
+silentremove(installation_path+"/lock/"+cpaneluser+".userdata")
 with open(installation_path+"/lock/"+cpaneluser+".userdata", 'w') as tempuserdata:
     json.dump(userdata_json, tempuserdata)
 print(("1 nDeploy:remove:pre:"+cpaneluser))
