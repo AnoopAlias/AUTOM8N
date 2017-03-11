4b70a4980d0e7589d72aa9a3f06c193a9e17c068
Anoop <anoop@yoga.gnusys.net>
2017-03-11 19:17:11 +0530

    bug fix

--- scripts/easy_hhvm_setup.sh
+++ scripts/easy_hhvm_setup.sh
@@ -9,10 +9,13 @@ if [ ${osversion} -le 6 ];then
   echo -e '\e[93m XtendWeb HHVM setup needs CentOS7 or CloudLinux7 as the OS \e[0m'
 else
   yum -y install epel-release
-  yum install libc-client-2007f cpp gcc-c++ cmake git psmisc {binutils,boost,jemalloc,numactl}-devel {ImageMagick,sqlite,tbb,bzip2,openldap,readline,elfutils-libelf,gmp,lz4,pcre}-devel lib{xslt,event,yaml,vpx,png,zip,icu,mcrypt,memcached,cap,dwarf}-devel {unixODBC,expat}-devel lib{edit,curl,xml2,xs
+  yum -y install libc-client-2007f cpp gcc-c++ cmake git psmisc {binutils,boost,jemalloc,numactl}-devel {ImageMagick,sqlite,tbb,bzip2,openldap,readline,elfutils-libelf,gmp,lz4,pcre}-devel
+  yum -y install lib{xslt,event,yaml,vpx,png,zip,icu,mcrypt,memcached,cap,dwarf}-devel {unixODBC,expat}-devel lib{edit,curl,xml2,xslt}-devel glog-devel oniguruma-devel ocaml gperf
+  yum -y install enca libjpeg-turbo-devel openssl-devel make
   yum -y install ${HHVM_RPM_LOCATION}
   /opt/nDeploy/scripts/update_backend.py add HHVM hhvm-3.15 systemd
-  echo "0 */6 * * * bash /opt/nDeploy/scripts/hhvm_ghost_hunter.sh" >> /etc/crontab
+  grep "hhvm_ghost_hunter.sh" /etc/crontab || echo "0 */6 * * * bash /opt/nDeploy/scripts/hhvm_ghost_hunter.sh" >> /etc/crontab
+  systemctl restart crond
   echo -e '\e[93m Be Warned that you are using binary HHVM from mirrors.linuxeye.com - which is not part of XtendWeb project \e[0m'
   echo -e '\e[93m HHVM setup : OK \e[0m'
 fi
