193c03cd77fe9da0a5f9a81ad8f527435b64ae71
Anoop <anoop@yoga.gnusys.net>
2017-04-05 12:55:26 +0530

    Fixes #188

--- rpm_buildtree/nDeploy-nginx_build_script.sh
+++ rpm_buildtree/nDeploy-nginx_build_script.sh
@@ -6,7 +6,7 @@
 #no sanitation is done as this would be mostly used by a person who knows what he is doing
 OSVERSION=$1
 NGINX_VERSION="1.11.13"
-NGINX_RPM_ITER="1.el${OSVERSION}"
+NGINX_RPM_ITER="2.el${OSVERSION}"
 NPS_VERSION="1.11.33.4"
 MY_RUBY_VERSION="2.3.1"
 PASSENGER_VERSION="5.1.2"
@@ -30,7 +30,7 @@ ZLIB_VERSION="1.2.11"
 rm -rf nginx-module-*
 rm -rf nginx-pkg
 rm -rf nginx-${NGINX_VERSION}*
-mkdir -p nginx-pkg/etc/nginx/{modules,modules.d,conf.auto}
+mkdir -p nginx-pkg/etc/nginx/{modules,modules.d,modules.debug,modules.debug.d,conf.auto}
 mkdir -p nginx-pkg/usr/nginx/scripts
 mkdir -p nginx-pkg/var/cache/nginx/ngx_pagespeed
 mkdir -p nginx-pkg/var/log/nginx
@@ -38,7 +38,7 @@ mkdir -p nginx-pkg/var/run
 
 for module in brotli geoip naxsi pagespeed passenger redis redis2 set_misc srcache_filter echo modsecurity
 do
-  mkdir -p nginx-module-${module}-pkg/etc/nginx/{modules,modules.d,conf.auto,conf.d}
+  mkdir -p nginx-module-${module}-pkg/etc/nginx/{modules,modules.d,modules.debug,modules.debug.d,conf.auto,conf.d}
   mkdir -p nginx-module-${module}-pkg/usr/nginx/scripts
 done
 mkdir -p nginx-module-naxsi-pkg/etc/nginx/naxsi.d
@@ -48,9 +48,9 @@ if [ ${OSVERSION} -eq 6 ];then
   rpm --import https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/slc6X/i386/RPM-GPG-KEY-cern
   wget -O /etc/yum.repos.d/slc6-devtoolset.repo https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/devtoolset/slc6-devtoolset.repo
   yum install devtoolset-2-gcc-c++ devtoolset-2-binutils
-  rsync -a --exclude 'usr/lib' --exclude 'etc/nginx/conf.d/modsecurity*' --exclude 'etc/nginx/naxsi.d/*' --exclude 'usr/nginx/scripts/*' --exclude 'etc/nginx/conf.d/naxsi_*' --exclude 'etc/nginx/conf.d/brotli.conf' --exclude 'etc/nginx/conf.d/pagespeed.conf' --exclude 'etc/nginx/conf.d/pagespeed_passthrough.conf' --exclude 'etc/nginx/fastcgi_params_geoip' --exclude 'etc/nginx/conf.auto/*' --exclude 'etc/nginx/modules/*' --exclude 'etc/nginx/modules.d/*' nginx-pkg-64-common/ nginx-pkg/
+  rsync -a --exclude 'usr/lib' --exclude 'etc/nginx/conf.d/modsecurity*' --exclude 'etc/nginx/naxsi.d/*' --exclude 'usr/nginx/scripts/*' --exclude 'etc/nginx/conf.d/naxsi_*' --exclude 'etc/nginx/conf.d/brotli.conf' --exclude 'etc/nginx/conf.d/pagespeed.conf' --exclude 'etc/nginx/conf.d/pagespeed_passthrough.conf' --exclude 'etc/nginx/fastcgi_params_geoip' --exclude 'etc/nginx/conf.auto/*' --exclude 'etc/nginx/modules.debug/*' --exclude 'etc/nginx/modules.debug.d/*' --exclude 'etc/nginx/modules/*' --exclude 'etc/nginx/modules.d/*' nginx-pkg-64-common/ nginx-pkg/
 else
-  rsync -a --exclude 'etc/rc.d' --exclude 'etc/nginx/conf.d/modsecurity*' --exclude 'etc/nginx/naxsi.d/*' --exclude 'usr/nginx/scripts/*' --exclude 'etc/nginx/conf.d/naxsi_*' --exclude 'etc/nginx/conf.d/brotli.conf' --exclude 'etc/nginx/conf.d/pagespeed.conf' --exclude 'etc/nginx/conf.d/pagespeed_passthrough.conf' --exclude 'etc/nginx/fastcgi_params_geoip' --exclude 'etc/nginx/conf.auto/*' --exclude 'etc/nginx/modules/*' --exclude 'etc/nginx/modules.d/*' nginx-pkg-64-common/ nginx-pkg/
+  rsync -a --exclude 'etc/rc.d' --exclude 'etc/nginx/conf.d/modsecurity*' --exclude 'etc/nginx/naxsi.d/*' --exclude 'usr/nginx/scripts/*' --exclude 'etc/nginx/conf.d/naxsi_*' --exclude 'etc/nginx/conf.d/brotli.conf' --exclude 'etc/nginx/conf.d/pagespeed.conf' --exclude 'etc/nginx/conf.d/pagespeed_passthrough.conf' --exclude 'etc/nginx/fastcgi_params_geoip' --exclude 'etc/nginx/conf.auto/*' --exclude 'etc/nginx/modules.debug/*' --exclude 'etc/nginx/modules.debug.d/*' --exclude 'etc/nginx/modules/*' --exclude 'etc/nginx/modules.d/*' nginx-pkg-64-common/ nginx-pkg/
 fi
 
 
@@ -76,6 +76,7 @@ sed -i 's/hash_init.max_size  = 1024/hash_init.max_size  = 256000/' naxsi-${NAXS
 #wget http://labs.frickle.com/files/ngx_cache_purge-${CACHE_PURGE_VERSION}.tar.gz
 #tar -xvzf ngx_cache_purge-${CACHE_PURGE_VERSION}.tar.gz
 
+# Pagespeed and brotli from google
 wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.zip
 unzip release-${NPS_VERSION}-beta.zip
 cd ngx_pagespeed-release-${NPS_VERSION}-beta/
@@ -83,8 +84,12 @@ wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
 tar -xzvf ${NPS_VERSION}.tar.gz
 cd ..
 
-wget https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
-tar -zxf openssl-${OPENSSL_VERSION}.tar.gz
+git clone https://github.com/google/ngx_brotli.git
+cd ngx_brotli && git submodule update --init && cd ..
+
+# LibreSSL , PCRE and ZLIB all latest versions
+#wget https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
+#tar -zxf openssl-${OPENSSL_VERSION}.tar.gz
 
 # LibreSSL
 wget https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${LIBRESSL_VERSION}.tar.gz
@@ -101,9 +106,6 @@ tar -zxf pcre-${PCRE_VERSION}.tar.gz
 # ZLIB
 wget http://zlib.net/zlib-${ZLIB_VERSION}.tar.gz
 tar -zxf zlib-${ZLIB_VERSION}.tar.gz
-
-git clone https://github.com/google/ngx_brotli.git
-cd ngx_brotli && git submodule update --init && cd ..
 
 # Modules from OpenResty project
 wget -O srcache-nginx-module.tgz https://github.com/openresty/srcache-nginx-module/archive/v${SRCACHE_NGINX_MODULE}.tar.gz
@@ -139,7 +141,8 @@ fi
 make DESTDIR=$(pwd)/tempostrip install
 strip --strip-debug ./tempostrip/usr/sbin/nginx
 rsync -a tempostrip/usr/sbin ../nginx-pkg/usr/
-#strip --strip-debug ./tempo/etc/nginx/modules/*.so
+strip --strip-debug ./tempostrip/etc/nginx/modules/*.so
+
 if [ ${OSVERSION} -eq 6 ];then
 ./configure --with-debug --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/etc/nginx/modules --with-pcre=./pcre-${PCRE_VERSION} --with-pcre-jit --with-zlib=./zlib-${ZLIB_VERSION} --with-openssl=./libressl-${LIBRESSL_VERSION} --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error_log --http-log-path=/var/log/nginx/access_log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nobody --group=nobody --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --add-dynamic-module=naxsi-${NAXSI_VERSION}/naxsi_src --with-file-aio --with-threads --with-stream --with-stream_ssl_module --with-http_slice_module  --with-compat --with-http_v2_module --with-http_geoip_module=dynamic --add-dynamic-module=ngx_pagespeed-release-${NPS_VERSION}-beta ${PS_NGX_EXTRA_FLAGS} --add-dynamic-module=/usr/local/rvm/gems/ruby-${MY_RUBY_VERSION}/gems/passenger-${PASSENGER_VERSION}/src/nginx_module --add-dynamic-module=ngx_brotli --add-dynamic-module=echo-nginx-module-${ECHO_NGINX_MODULE} --add-dynamic-module=headers-more-nginx-module-${HEADERS_MORE_NGINX_MODULE} --add-dynamic-module=ngx_http_redis-${REDIS_NGINX_MODULE} --add-dynamic-module=redis2-nginx-module --add-dynamic-module=srcache-nginx-module-${SRCACHE_NGINX_MODULE} --add-dynamic-module=ngx_devel_kit-${NGX_DEVEL_KIT} --add-dynamic-module=set-misc-nginx-module-${SET_MISC_NGINX_MODULE} --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic' --with-ld-opt="-Wl,-E -lrt"
 else
@@ -164,26 +167,35 @@ rsync -a ../nginx-pkg-64-common/etc/nginx/conf.d/pagespeed_passthrough.conf ../n
 rsync -a ../nginx-pkg-64-common/etc/nginx/conf.d/brotli.conf ../nginx-module-brotli-pkg/etc/nginx/conf.d/
 rsync -a ../nginx-pkg-64-common/etc/nginx/conf.d/naxsi_* ../nginx-module-naxsi-pkg/etc/nginx/conf.d/
 rsync -a ../nginx-pkg-64-common/etc/nginx/conf.d/modsecurity.conf ../nginx-module-modsecurity-pkg/etc/nginx/conf.d/
-#rsync -a tempo/usr/sbin ../nginx-pkg/usr/
+
 for module in brotli geoip naxsi pagespeed passenger redis redis2 set_misc srcache_filter echo modsecurity
 do
-  rsync -a tempo/etc/nginx/modules/ngx_http_${module}* ../nginx-module-${module}-pkg/etc/nginx/modules/
+  rsync -a tempostrip/etc/nginx/modules/ngx_http_${module}* ../nginx-module-${module}-pkg/etc/nginx/modules/
+  rsync -a tempo/etc/nginx/modules/ngx_http_${module}* ../nginx-module-${module}-pkg/etc/nginx/modules.debug/
   if [ -f ../nginx-pkg-64-common/etc/nginx/conf.auto/${module}.conf ] ; then
     rsync -a ../nginx-pkg-64-common/etc/nginx/conf.auto/${module}.conf ../nginx-module-${module}-pkg/etc/nginx/conf.auto/
   fi
   rsync -a ../nginx-pkg-64-common/etc/nginx/modules.d/${module}.load ../nginx-module-${module}-pkg/etc/nginx/modules.d/
+  rsync -a ../nginx-pkg-64-common/etc/nginx/modules.debug.d/${module}.load ../nginx-module-${module}-pkg/etc/nginx/modules.debug.d/
 done
-rsync -a tempo/etc/nginx/modules/ngx_pagespeed.so ../nginx-module-pagespeed-pkg/etc/nginx/modules/
-rsync -a tempo/etc/nginx/modules/ndk_http_module.so ../nginx-pkg/etc/nginx/modules/
+rsync -a tempostrip/etc/nginx/modules/ngx_pagespeed.so ../nginx-module-pagespeed-pkg/etc/nginx/modules/
+rsync -a tempo/etc/nginx/modules/ngx_pagespeed.so ../nginx-module-pagespeed-pkg/etc/nginx/modules.debug/
+rsync -a tempostrip/etc/nginx/modules/ndk_http_module.so ../nginx-pkg/etc/nginx/modules/
+rsync -a tempo/etc/nginx/modules/ndk_http_module.so ../nginx-pkg/etc/nginx/modules.debug/
 rsync -a ../nginx-pkg-64-common/etc/nginx/modules.d/ndk.load ../nginx-pkg/etc/nginx/modules.d/
-rsync -a tempo/etc/nginx/modules/ngx_http_headers_more_filter_module.so ../nginx-pkg/etc/nginx/modules/
+rsync -a ../nginx-pkg-64-common/etc/nginx/modules.debug.d/ndk.load ../nginx-pkg/etc/nginx/modules.debug.d/
+rsync -a tempostrip/etc/nginx/modules/ngx_http_headers_more_filter_module.so ../nginx-pkg/etc/nginx/modules/
+rsync -a tempo/etc/nginx/modules/ngx_http_headers_more_filter_module.so ../nginx-pkg/etc/nginx/modules.debug/
 rsync -a ../nginx-pkg-64-common/etc/nginx/modules.d/headers_more_filter.load ../nginx-pkg/etc/nginx/modules.d/
+rsync -a ../nginx-pkg-64-common/etc/nginx/modules.debug.d/headers_more_filter.load ../nginx-pkg/etc/nginx/modules.debug.d/
 mv ../nginx-module-modsecurity-pkg/etc/nginx/modules.d/modsecurity.load ../nginx-module-modsecurity-pkg/etc/nginx/modules.d/zz_modsecurity.load
+mv ../nginx-module-modsecurity-pkg/etc/nginx/modules.debug.d/modsecurity.load ../nginx-module-modsecurity-pkg/etc/nginx/modules.debug.d/zz_modsecurity.load
 
 rsync -a ../nginx-pkg-64-common/usr/nginx/scripts/nginx-passenger* ../nginx-module-passenger-pkg/usr/nginx/scripts/
 rsync -a ../nginx-pkg-64-common/usr/nginx/scripts/nxapi* ../nginx-module-naxsi-pkg/usr/nginx/scripts/
 #We remove redis2 from redis package which gets copied due to file glob
 rm -f ../nginx-module-redis-pkg/etc/nginx/modules/ngx_http_redis2_module.so
+rm -f ../nginx-module-redis-pkg/etc/nginx/modules.debug/ngx_http_redis2_module.so
 
 sed -i "s/RUBY_VERSION/$MY_RUBY_VERSION/g" ../nginx-module-passenger-pkg/etc/nginx/conf.auto/passenger.conf
 sed -i "s/PASSENGER_VERSION/$PASSENGER_VERSION/g" ../nginx-module-passenger-pkg/etc/nginx/conf.auto/passenger.conf
@@ -200,17 +212,17 @@ for module in brotli geoip naxsi pagespeed passenger redis redis2 set_misc srcac
 do
   cd ../nginx-module-${module}-pkg
   if [ ${module} == "brotli" ];then
-    fpm -s dir -t rpm -C ../nginx-module-${module}-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com --description "nDeploy custom nginx-${module} package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx-module-${module} -d libbrotli-nDeploy --name nginx-nDeploy-module-${module} .
+    fpm -s dir -t rpm -C ../nginx-module-${module}-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com --description "nDeploy custom nginx-${module} package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx-module-${module} -d libbrotli-nDeploy -d nginx-nDeploy --name nginx-nDeploy-module-${module} .
   elif [ ${module} == "modsecurity" ];then
     if [ ${OSVERSION} -gt 6 ];then
-      fpm -s dir -t rpm -C ../nginx-module-${module}-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com --description "nDeploy custom nginx-${module} package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx-module-${module} -d libmodsecurity-nDeploy --name nginx-nDeploy-module-${module} .
+      fpm -s dir -t rpm -C ../nginx-module-${module}-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com --description "nDeploy custom nginx-${module} package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx-module-${module} -d libmodsecurity-nDeploy -d nginx-nDeploy --name nginx-nDeploy-module-${module} .
     fi
   elif [ ${module} == "geoip" ];then
-    fpm -s dir -t rpm -C ../nginx-module-${module}-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com --description "nDeploy custom nginx-${module} package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx-module-${module} -d GeoIP --name nginx-nDeploy-module-${module} .
+    fpm -s dir -t rpm -C ../nginx-module-${module}-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com --description "nDeploy custom nginx-${module} package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx-module-${module} -d GeoIP -d nginx-nDeploy --name nginx-nDeploy-module-${module} .
   elif [ ${module} == "pagespeed" ];then
-    fpm -s dir -t rpm -C ../nginx-module-${module}-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com --description "nDeploy custom nginx-${module} package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx-module-${module} -d memcached --name nginx-nDeploy-module-${module} .
+    fpm -s dir -t rpm -C ../nginx-module-${module}-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com --description "nDeploy custom nginx-${module} package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx-module-${module} -d memcached -d nginx-nDeploy --name nginx-nDeploy-module-${module} .
   else
-    fpm -s dir -t rpm -C ../nginx-module-${module}-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com --description "nDeploy custom nginx-${module} package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx-module-${module} --name nginx-nDeploy-module-${module} .
+    fpm -s dir -t rpm -C ../nginx-module-${module}-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com --description "nDeploy custom nginx-${module} package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx-module-${module} -d nginx-nDeploy --name nginx-nDeploy-module-${module} .
   fi
   rsync -a nginx-nDeploy-* root@gnusys.net:/usr/share/nginx/html/CentOS/${OSVERSION}/x86_64/
 done
