001108f4149e3d1c4123f5e97bc702988045e7b4
Anoop <anoop@yoga.gnusys.net>
2017-04-09 13:05:13 +0530

    misc fix

--- rpm_buildtree/nDeploy_build_script.sh
+++ rpm_buildtree/nDeploy_build_script.sh
@@ -2,7 +2,7 @@
 #Author: Anoop P Alias
 
 NDEPLOY_VERSION="4.2"
-RPM_ITERATION="21"
+RPM_ITERATION="22"
 
 rm -f nDeploy-pkg/nDeploy-* nDeploy-pkg-centos7/nDeploy-*
 rsync -av ../scripts/ nDeploy-pkg/opt/nDeploy/scripts/
--- scripts/setup_additional_templates.sh
+++ scripts/setup_additional_templates.sh
@@ -9,7 +9,7 @@ else
   service redis start && chkconfig redis on
 fi
 
-yum --enablerepo=ndeploy -y install nginx-nDeploy-module-redis2 nginx-nDeploy-module-redis nginx-nDeploy-module-echo nginx-nDeploy-module-set_misc nginx-nDeploy-module-srcache_filter
+rpm -q openresty-nDeploy || yum --enablerepo=ndeploy -y install nginx-nDeploy-module-redis2 nginx-nDeploy-module-redis nginx-nDeploy-module-echo nginx-nDeploy-module-set_misc nginx-nDeploy-module-srcache_filter
 
 /opt/nDeploy/scripts/update_profiles.py add root main PHP wpredis_helper.j2 "Wordpress+Redis+nginx-helper"
 /opt/nDeploy/scripts/update_profiles.py add root main HHVM wpredis_helper.j2 "Wordpress+Redis+nginx-helper"
