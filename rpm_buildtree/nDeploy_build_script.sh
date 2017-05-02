50d4c2e26166d00cd7238f4102365fbc0ecd8e8f
Anoop <anoop@yoga.gnusys.net>
2017-05-02 13:05:15 +0530

    playfixes

--- rpm_buildtree/nDeploy_build_script.sh
+++ rpm_buildtree/nDeploy_build_script.sh
@@ -2,7 +2,7 @@
 #Author: Anoop P Alias
 
 NDEPLOY_VERSION="4.3"
-RPM_ITERATION="11"
+RPM_ITERATION="12"
 
 rm -f nDeploy-pkg/nDeploy-* nDeploy-pkg-centos7/nDeploy-*
 rsync -av ../scripts/ nDeploy-pkg/opt/nDeploy/scripts/
