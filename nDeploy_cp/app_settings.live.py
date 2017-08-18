269a4048ce3344edf42a6af940c4feac7a68acd6
Anoop <anoop@yoga.gnusys.net>
2017-08-18 20:08:14 +0530

    UI

--- nDeploy_cp/app_settings.live.py
+++ nDeploy_cp/app_settings.live.py
@@ -179,7 +179,7 @@ if form.getvalue('domain'):
         print('<form id="config" class="form-inline config-save" action="passenger_module_installer.live.py" method="post">')
         print('<ul class="list-group">')
         if backend_category == "RUBY":
-            print(('<div class="alert alert-info alert-top">Detected <span class="label label-info">RUBY</span> project, specify your dependencies in <br> <kbd>'+ document_root +'/Gemfile</kbd></div>'))
+            print(('<div class="alert alert-info alert-top">Detected <span class="label label-info">RUBY</span> project, specify your dependencies in <br><br><br> <kbd>'+ document_root +'/Gemfile</kbd></div>'))
         print(('<input class="hidden" name="domain" value="'+mydomain+'">'))
         print('<input class="btn btn-primary" type="submit" value="INSTALL DEPS">')
         print('</form>')
