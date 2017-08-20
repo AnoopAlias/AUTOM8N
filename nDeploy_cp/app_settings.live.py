b3a6aa6dbb7e6bcd37bf2f4244a03ff799e3f68d
Anoop <anoop@yoga.gnusys.net>
2017-08-20 17:00:15 +0530

    UI

--- nDeploy_cp/app_settings.live.py
+++ nDeploy_cp/app_settings.live.py
@@ -135,7 +135,7 @@ if form.getvalue('domain'):
             else:
                 print(('<div class="alert alert-info alert-top">Nginx is proxying to <span class="label label-info">'+backend_version+'</span> with settings  <span class="label label-info">'+apptemplate_description+'</span></div>'))
         else:
-            print(('<div class="alert alert-info alert-top">You have deployed  <span class="label label-info">'+apptemplate_description+'</span> project on native <span class="label label-info">NGINX</span> with <span class="label label-info">'+backend_category+'</span> <span class="label label-info">'+backend_version+'</span> application server</div>'))
+            print(('<div class="alert alert-info alert-top">Your current project is <span class="label label-info">'+apptemplate_description+'</span> on native <span class="label label-info">NGINX</span> with <span class="label label-info">'+backend_category+'</span> <span class="label label-info">'+backend_version+'</span> application server</div>'))
         print('</ul>')
         print('<p><em>To change application server select a BACKEND from the drop down below and click submit:</em></p>')
         print('<select name="backend">')
