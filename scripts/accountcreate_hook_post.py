ee274db632021960b0f9f6e270c4c0c8969d13c3
Anoop <anoop@yoga.gnusys.net>
2017-09-10 10:56:00 +0530

    Fixes #275

--- scripts/accountcreate_hook_post.py
+++ scripts/accountcreate_hook_post.py
@@ -31,8 +31,8 @@ hostingplan = mydict["plan"]
 hostingplan_filename = hostingplan.replace(" ", "_")
 domain_data_file = installation_path+"/domain-data/"+cpaneldomain
 if not os.path.isfile(domain_data_file):
-    if os.path.isfile(installation_path+"/conf/domain_data_default_"+hostingplan_filename+".yaml"):
-        TEMPLATE_FILE = installation_path+"/conf/domain_data_default_"+hostingplan_filename+".yaml"
+    if os.path.isfile(installation_path+"/conf/domain_data_default_local_"+hostingplan_filename+".yaml"):
+        TEMPLATE_FILE = installation_path+"/conf/domain_data_default_local_"+hostingplan_filename+".yaml"
     shutil.copyfile(TEMPLATE_FILE, domain_data_file)
     cpuser_uid = pwd.getpwnam(cpaneluser).pw_uid
     cpuser_gid = grp.getgrnam(cpaneluser).gr_gid
