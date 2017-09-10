b25c04388a4063a28df1d2c94d0253c7e272902e
Anoop <anoop@yoga.gnusys.net>
2017-09-10 10:53:46 +0530

    Fixes #275

--- scripts/accountcreate_hook_post.py
+++ scripts/accountcreate_hook_post.py
@@ -5,6 +5,8 @@ import sys
 import subprocess
 import os
 import pwd
+import grp
+import shutil
 try:
     import simplejson as json
 except ImportError:
@@ -24,11 +26,22 @@ cluster_config_file = installation_path+"/conf/ndeploy_cluster.yaml"
 cpjson = json.load(sys.stdin)
 mydict = cpjson["data"]
 cpaneluser = mydict["user"]
+cpaneldomain = mydict["domain"]
+hostingplan = mydict["plan"]
+hostingplan_filename = hostingplan.replace(" ", "_")
+domain_data_file = installation_path+"/domain-data/"+cpaneldomain
+if not os.path.isfile(domain_data_file):
+    if os.path.isfile(installation_path+"/conf/domain_data_default_"+hostingplan_filename+".yaml"):
+        TEMPLATE_FILE = installation_path+"/conf/domain_data_default_"+hostingplan_filename+".yaml"
+    shutil.copyfile(TEMPLATE_FILE, domain_data_file)
+    cpuser_uid = pwd.getpwnam(cpaneluser).pw_uid
+    cpuser_gid = grp.getgrnam(cpaneluser).gr_gid
+    os.chown(domain_data_file, cpuser_uid, cpuser_gid)
+    os.chmod(domain_data_file, 0o660)
 user_shell = pwd.getpwnam(cpaneluser).pw_shell
 # If nDeploy cluster is enabled we need to add users,DNS entry for the same
 if os.path.exists(cluster_config_file):
     cpaneluserhome = mydict["homedir"]
-    cpaneldomain = mydict["domain"]
     # Calling ansible ad-hoc command to create users across the cluster
     # Using subprocess.call here as we are not in a hurry and no async call is required
     subprocess.call('ansible -i /opt/nDeploy/conf/nDeploy-cluster/hosts ndeployslaves -m user -a "name='+cpaneluser+' home='+cpaneluserhome+' shell='+user_shell+'"', shell=True)
