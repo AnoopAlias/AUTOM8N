ae1f0a8f4d9b1f76022fa78e5e913990ff75ebed
Anoop <anoop@yoga.gnusys.net>
2017-10-11 16:45:17 +0530

    Testing workaround SpiderLabs/ModSecurity-nginx#67

--- rpm_buildtree/nDeploy-libmodsecurity_build_script.centos7.sh
+++ rpm_buildtree/nDeploy-libmodsecurity_build_script.centos7.sh
@@ -10,6 +10,14 @@ yum install gcc-c++ flex bison yajl yajl-devel curl-devel curl GeoIP-devel doxyg
 git clone https://github.com/SpiderLabs/ModSecurity
 cd ModSecurity
 git checkout -b v3/master origin/v3/master
+#Temporary patch for memory issue Ref: https://github.com/SpiderLabs/ModSecurity-nginx/issues/67
+#wget -O memory.zip https://github.com/SpiderLabs/ModSecurity/files/1333500/memory.zip
+#unzip memory.zip
+#patch -p1 < ./memory.patch
+wget -O rules.zip https://github.com/SpiderLabs/ModSecurity/files/1348565/rules.zip
+unzip rules.zip
+rsync -av rules.h ./headers/modsecurity/
+rsync -av rules.cc ./src/
 sh build.sh
 git submodule init
 git submodule update
