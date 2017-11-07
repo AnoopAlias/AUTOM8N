f777882f55d8e5f064723dc74ed60440ae8e8178
Anoop P Alias <anoopalias01@gmail.com>
2017-11-07 12:34:36 +0530

    version bump

--- rpm_buildtree/nDeploy_build_script.sh
+++ rpm_buildtree/nDeploy_build_script.sh
@@ -2,7 +2,7 @@
 #Author: Anoop P Alias
 
 NDEPLOY_VERSION="4.3"
-RPM_ITERATION="39"
+RPM_ITERATION="40"
 
 yum install gcc-c++ flex bison yajl yajl-devel curl-devel curl GeoIP-devel doxygen zlib-devel pcre-devel rpm-build
 
@@ -19,12 +19,12 @@ rsync -av ../PHPfpmSelector/ nDeploy-pkg-centos7/opt/nDeploy/PHPfpmSelector/
 
 cd nDeploy-pkg
 mkdir opt/nDeploy/lock
-fpm -s dir -t rpm -C ../nDeploy-pkg --vendor "Anoop P Alias" --version ${NDEPLOY_VERSION} --iteration ${RPM_ITERATION}.el6 -d python-psutil -d python-inotify -d python-jinja2 -d python-simplejson -d python-argparse -d PyYAML -d python-lxml -d python-configparser -a noarch -m anoopalias01@gmail.com -e --description "nDeploy cPanel plugin" --url http://anoopalias.github.io/XtendWeb/ --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
+fpm -s dir -t rpm -C ../nDeploy-pkg --vendor "Anoop P Alias" --version ${NDEPLOY_VERSION} --iteration ${RPM_ITERATION}.el6 -d python-psutil -d python-inotify -d python-jinja2 -d python-simplejson -d python-argparse -d PyYAML -d python-lxml -d python-configparser -a noarch -m anoopalias01@gmail.com -e --description "nDeploy cPanel plugin" --url http://anoopalias.github.io/XtendWeb/ --before-install ../before_ndeploy_install --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
 rsync -av nDeploy-* root@autom8n.com:/usr/share/nginx/autom8n/CentOS/6/x86_64/
 
 cd ..
 cd nDeploy-pkg-centos7
 mkdir opt/nDeploy/lock
-fpm -s dir -t rpm -C ../nDeploy-pkg-centos7 --vendor "Anoop P Alias" --version ${NDEPLOY_VERSION} --iteration ${RPM_ITERATION}.el7 -d python-psutil -d python-inotify -d python-jinja2 -d python-simplejson -d python-argparse -d PyYAML -d python-lxml -d python-configparser -a noarch -m anoopalias01@gmail.com -e --description "nDeploy cPanel plugin" --url http://anoopalias.github.io/XtendWeb/ --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
+fpm -s dir -t rpm -C ../nDeploy-pkg-centos7 --vendor "Anoop P Alias" --version ${NDEPLOY_VERSION} --iteration ${RPM_ITERATION}.el7 -d python-psutil -d python-inotify -d python-jinja2 -d python-simplejson -d python-argparse -d PyYAML -d python-lxml -d python-configparser -a noarch -m anoopalias01@gmail.com -e --description "nDeploy cPanel plugin" --url http://anoopalias.github.io/XtendWeb/ --before-install ../before_ndeploy_install --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
 rsync -av nDeploy-* root@autom8n.com:/usr/share/nginx/autom8n/CentOS/7/x86_64/
 cd ..
