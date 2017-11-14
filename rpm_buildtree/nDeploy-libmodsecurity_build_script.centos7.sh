fda6f469a796bf735e1a2554f12f7860a76c12ef
Anoop P Alias <anoopalias01@gmail.com>
2017-11-14 11:05:46 +0530

    ssdeep add on libmodsecurity

--- rpm_buildtree/nDeploy-libmodsecurity_build_script.centos7.sh
+++ rpm_buildtree/nDeploy-libmodsecurity_build_script.centos7.sh
@@ -6,16 +6,16 @@ MODSEC_VERSION="3.0"
 MODSEC_RPM_ITER="13.el7"
 
 rm -rf ModSecurity*
-yum install gcc-c++ flex bison yajl yajl-devel curl-devel curl GeoIP-devel doxygen zlib-devel pcre-devel rpm-build
+yum install gcc-c++ flex bison yajl yajl-devel curl-devel curl GeoIP-devel doxygen zlib-devel pcre-devel rpm-build ssdeep*
 git clone https://github.com/SpiderLabs/ModSecurity
 cd ModSecurity
 git checkout -b v3/master origin/v3/master
 sh build.sh
 git submodule init
 git submodule update
-./configure --prefix=/opt/nDeploy-libmodsecurity
+./configure -without-lmdb --prefix=/opt/nDeploy-libmodsecurity
 make DESTDIR=$(pwd)/tempo install
 cd tempo
 
-fpm -s dir -t rpm -C ../tempo --vendor "Anoop P Alias" --version ${MODSEC_VERSION} --iteration ${MODSEC_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com -e --description "nDeploy custom libmodsecurity package" --url https://github.com/SpiderLabs/ModSecurity -d libcurl -d GeoIP -d yajl -d libxml2 --name libmodsecurity-nDeploy .
+fpm -s dir -t rpm -C ../tempo --vendor "Anoop P Alias" --version ${MODSEC_VERSION} --iteration ${MODSEC_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com -e --description "nDeploy custom libmodsecurity package" --url https://github.com/SpiderLabs/ModSecurity -d libcurl -d GeoIP -d yajl -d libxml2 -d ssdeep --name libmodsecurity-nDeploy .
 rsync -av libmodsecurity-nDeploy-* root@autom8n.com:/usr/share/nginx/autom8n/CentOS/7/x86_64/
