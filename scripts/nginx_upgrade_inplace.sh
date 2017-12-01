1d01fac9ceb23108a0600fac704eec939279f735
Anoop P Alias <anoopalias01@gmail.com>
2017-12-01 20:13:47 +0530

    Add message in script

--- scripts/nginx_upgrade_inplace.sh
+++ scripts/nginx_upgrade_inplace.sh
@@ -3,3 +3,4 @@ kill -s USR2 $(cat /var/run/nginx.pid)
 kill -s WINCH $(cat /var/run/nginx.pid.oldbin)
 kill -s QUIT $(cat /var/run/nginx.pid.oldbin)
 kill -s HUP $(cat /var/run/nginx.pid)
+echo -e '\e[93m Nginx binary in-place upgrade: OK \e[0m'
