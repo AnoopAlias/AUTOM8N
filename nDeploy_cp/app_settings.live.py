1c81092b580b5a72ea56c18e9996088e686b6ee8
Anoop P Alias <anoopalias01@gmail.com>
2017-12-07 12:58:03 +0530

    fix

--- nDeploy_cp/app_settings.live.py
+++ nDeploy_cp/app_settings.live.py
@@ -695,11 +695,9 @@ if form.getvalue('domain'):
         print('<div class="alert alert-danger"><span class="glyphicon glyphicon-alert" aria-hidden="true"></span> domain-data file i/o error</div>')
 else:
     print('<div class="alert alert-danger"><span class="glyphicon glyphicon-alert" aria-hidden="true"></span> Forbidden</div>')
-print('<div class="panel panel-default">')
-print('<div class="panel-body">')
+print('<ol class="breadcrumb">')
 print('<div class="panel-footer"><small>Need Help <span class="glyphicon glyphicon-circle-arrow-right" aria-hidden="true"></span> <a target="_blank" href="https://autom8n.com/xtendweb/UserDocs.html">XtendWeb Docs</a></small></div>')
-print('</div>')
-print('</div>')
+print('</ol>')
 
 print('</div>')  # div3
 print('</div>')  # div2
