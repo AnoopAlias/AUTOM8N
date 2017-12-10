f4c694ab7593f3b19a34870ff1b2b58f670dab54
Anoop P Alias <anoopalias01@gmail.com>
2017-12-10 12:46:16 +0530

    test

--- nDeploy_cp/app_settings.live.py
+++ nDeploy_cp/app_settings.live.py
@@ -726,12 +726,12 @@ if form.getvalue('domain'):
         # Pass on the domain name to the next stage
         print('<ul class="list-group">')
         # print('<li class="list-group-item">')
-        print('<div class="row">')
+        # print('<div class="row">')
         print('<li class="list-group-item">')
         print(('<input style="display:none" name="domain" value="'+mydomain+'">'))
         print('<input class="btn btn-primary" type="submit" value="Submit">')
         print('</li>')
-        print('</div>')
+        # print('</div>')
         # print('</li>')
         print('</ul>')
 
