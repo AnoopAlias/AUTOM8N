408e77bd9b6555d823ef7b8ba2fd1f0440ceba15
Anoop P Alias <anoopalias01@gmail.com>
2017-12-12 12:06:45 +0530

    template name changes

--- scripts/setup_full_page_cache_templates.sh
+++ scripts/setup_full_page_cache_templates.sh
@@ -12,20 +12,20 @@ fi
 rpm -q openresty-nDeploy || yum --enablerepo=ndeploy -y install nginx-nDeploy-module-redis2 nginx-nDeploy-module-redis nginx-nDeploy-module-echo nginx-nDeploy-module-set_misc nginx-nDeploy-module-srcache_filter
 
 if [ -f /opt/nDeploy/scripts/update_profiles.py ];then
-  /opt/nDeploy/scripts/update_profiles.py add root main PHP wpredis_helper.j2 "Wordpress+Redis+nginx-helper"
-  /opt/nDeploy/scripts/update_profiles.py add root main PHP wpredis_helper_cluster.j2 "Wordpress+Redis+nginx-helper(cluster)"
-  /opt/nDeploy/scripts/update_profiles.py add root main HHVM wpredis_helper.j2 "Wordpress+Redis+nginx-helper"
-  /opt/nDeploy/scripts/update_profiles.py add root main HHVM wpredis_helper_cluster.j2 "Wordpress+Redis+nginx-helper(cluster)"
+  /opt/nDeploy/scripts/update_profiles.py add root main PHP wpredis_helper.j2 "Wordpress-FullPage-Cache+nginx-helper"
+  /opt/nDeploy/scripts/update_profiles.py del root main PHP wpredis_helper_cluster.j2 "Wordpress-FullPage-Cache+nginx-helper(cluster)"
+  /opt/nDeploy/scripts/update_profiles.py add root main HHVM wpredis_helper.j2 "Wordpress-FullPage-Cache+nginx-helper"
+  /opt/nDeploy/scripts/update_profiles.py del root main HHVM wpredis_helper_cluster.j2 "Wordpress-FullPage-Cache+nginx-helper(cluster)"
 
-  /opt/nDeploy/scripts/update_profiles.py add root main PHP wpredis_auto.j2 "Wordpress+Redis"
-  /opt/nDeploy/scripts/update_profiles.py add root main HHVM wpredis_auto.j2 "Wordpress+Redis"
-  /opt/nDeploy/scripts/update_profiles.py add root main PHP wpredis_auto_cluster.j2 "Wordpress+Redis(cluster)"
-  /opt/nDeploy/scripts/update_profiles.py add root main HHVM wpredis_auto_cluster.j2 "Wordpress+Redis(cluster)"
+  /opt/nDeploy/scripts/update_profiles.py add root main PHP wpredis_auto.j2 "Wordpress-FullPage-Cahe"
+  /opt/nDeploy/scripts/update_profiles.py add root main HHVM wpredis_auto.j2 "Wordpress-FullPage-Cache"
+  /opt/nDeploy/scripts/update_profiles.py del root main PHP wpredis_auto_cluster.j2 "Wordpress-FullPage-Cache(cluster)"
+  /opt/nDeploy/scripts/update_profiles.py del root main HHVM wpredis_auto_cluster.j2 "Wordpress-FullPage-Cache(cluster)"
 
-  /opt/nDeploy/scripts/update_profiles.py add root main PHP drupalredis_auto.j2 "Drupal+Redis"
-  /opt/nDeploy/scripts/update_profiles.py add root main HHVM drupalredis_auto.j2 "Drupal+Redis"
-  /opt/nDeploy/scripts/update_profiles.py add root main PHP drupalredis_auto_cluster.j2 "Drupal+Redis(cluster)"
-  /opt/nDeploy/scripts/update_profiles.py add root main HHVM drupalredis_auto_cluster.j2 "Drupal+Redis(cluster)"
+  /opt/nDeploy/scripts/update_profiles.py add root main PHP drupalredis_auto.j2 "Drupal-FullPage-Cache"
+  /opt/nDeploy/scripts/update_profiles.py add root main HHVM drupalredis_auto.j2 "Drupal-FullPage-Cache"
+  /opt/nDeploy/scripts/update_profiles.py del root main PHP drupalredis_auto_cluster.j2 "Drupal-FullPage-Cache(cluster)"
+  /opt/nDeploy/scripts/update_profiles.py del root main HHVM drupalredis_auto_cluster.j2 "Drupal-FullPage-Cache(cluster)"
 fi
 
-echo -e '\e[93m Additional templates setup: OK \e[0m'
+echo -e '\e[93m FullPage cache templates setup: OK \e[0m'
