bd81710f465f80e9dd27ce9b7db2260a6f928e61
Anoop P Alias <anoopalias01@gmail.com>
2017-12-17 16:47:16 +0530

    fix

--- nDeploy_cp/view_log.live.py
+++ nDeploy_cp/view_log.live.py
@@ -60,7 +60,7 @@ print('<li><a href="xtendweb.live.py">Select Domain</a></li><li class="active">V
 print('</ol>')
 php_log = cpanelhome+"/logs/php_error_log"
 if os.path.isfile(php_log):
-    tail_cmd = '/usr/bin/tail -10 '+cpaneluser
+    tail_cmd = '/usr/bin/tail -10 '+php_log
     run_tail_cmd = subprocess.Popen(tail_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, err = run_tail_cmd.communicate()
     if output:
