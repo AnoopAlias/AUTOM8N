0f7fd0047b2acdf4ff91d0ffefdd99be3558a8c8
Anoop P Alias <anoopalias01@gmail.com>
2018-01-03 11:13:14 +0530

    mark naxsi module as conflict

--- rpm_buildtree/nDeploy-nginx_build_script.sh
+++ rpm_buildtree/nDeploy-nginx_build_script.sh
@@ -209,7 +209,7 @@ sed -i "s/PASSENGER_VERSION/$PASSENGER_VERSION/g" ../nginx-module-passenger-pkg/
 rsync -a /usr/local/rvm/gems/ruby-${MY_RUBY_VERSION}/gems/passenger-${PASSENGER_VERSION}/buildout ../nginx-module-passenger-pkg/usr/nginx/
 cd ../nginx-pkg
 
-fpm -s dir -t rpm -C ../nginx-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com --description "nDeploy custom nginx package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx --conflicts openresty-nDeploy --after-install ../after_nginx_install --name nginx-nDeploy .
+fpm -s dir -t rpm -C ../nginx-pkg --vendor "Anoop P Alias" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m anoopalias01@gmail.com --description "nDeploy custom nginx package" --url http://anoopalias.github.io/XtendWeb/ --conflicts nginx --conflicts openresty-nDeploy --conflicts nginx-nDeploy-module-naxsi --after-install ../after_nginx_install --name nginx-nDeploy .
 rsync -a nginx-nDeploy-* root@autom8n.com:/usr/share/nginx/autom8n/CentOS/${OSVERSION}/x86_64/
 
 for module in brotli geoip pagespeed passenger redis redis2 set_misc srcache_filter echo modsecurity testcookie_access
