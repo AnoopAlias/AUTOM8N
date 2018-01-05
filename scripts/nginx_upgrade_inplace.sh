987addc1717fb1a53aeb46bcec8618b592f498be
Anoop P Alias <anoopalias01@gmail.com>
2018-01-05 12:03:16 +0530

    sleep time

--- scripts/nginx_upgrade_inplace.sh
+++ scripts/nginx_upgrade_inplace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kill -s USR2 $(cat /var/run/nginx.pid)
-sleep 5
+sleep 10
 kill -s WINCH $(cat /var/run/nginx.pid.oldbin)
 kill -s QUIT $(cat /var/run/nginx.pid.oldbin)
 kill -s HUP $(cat /var/run/nginx.pid)
