5d95ef35b54bb8feb7ef5ce4ac17a7f2f0dc9686
Anoop P Alias <anoopalias01@gmail.com>
2018-01-08 21:24:51 +0530

    remove the buoldtree

--- rpm_buildtree/nDeploy-release_build_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Author: Anoop P Alias
-
-cd nDeploy-release-centos-pkg
-fpm -s dir -t rpm -C ../nDeploy-release-centos-pkg --vendor "Anoop P Alias" --iteration 6 -a noarch -m anoopalias01@gmail.com -e --description "nDeploy rpm repo" --url http://anoopalias.github.io/XtendWeb/ --after-install ../after_ndeploy_release_install --name nDeploy-release-centos .
-cd ..
