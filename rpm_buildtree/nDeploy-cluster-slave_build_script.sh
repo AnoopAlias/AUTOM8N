7db354d7a1f0eed1fcda8705ce0813994153ecb6
Anoop P Alias <anoopalias01@gmail.com>
2018-01-08 13:50:00 +0530

    ver bump

--- rpm_buildtree/nDeploy-cluster-slave_build_script.sh
+++ rpm_buildtree/nDeploy-cluster-slave_build_script.sh
@@ -2,7 +2,7 @@
 #Author: Anoop P Alias
 
 NDEPLOY_VERSION="4.3"
-RPM_ITERATION="58"
+RPM_ITERATION="60"
 
 yum install gcc-c++ flex bison yajl yajl-devel curl-devel curl GeoIP-devel doxygen zlib-devel pcre-devel rpm-build
 
