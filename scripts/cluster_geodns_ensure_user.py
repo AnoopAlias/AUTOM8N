9ab6144b55a55b9ff49945e2f59e92eb43dc4908
Anoop P Alias <anoopalias01@gmail.com>
2018-02-27 13:53:19 +0530

    ongoing #293

--- scripts/cluster_geodns_ensure_user.py
+++ scripts/cluster_geodns_ensure_user.py
@@ -54,7 +54,7 @@ def cluster_ensure_zone(zone_name, hostname, domain_ip):
                 the_geozone["data"][rr["name"].replace("."+zone_name+".", "")] = {}
                 the_geozone["data"][rr["name"].replace("."+zone_name+".", "")]["txt"] = []
         elif rr["type"] == "CNAME":
-            the_geozone["data"][rr["name"]] = {}
+            the_geozone["data"][rr["name"].replace("."+zone_name+".", "")] = {}
     # Populate the Json data structure
     for rr in resource_record:
         # Lets deal with NS records first
@@ -82,7 +82,7 @@ def cluster_ensure_zone(zone_name, hostname, domain_ip):
             else:
                 the_geozone["data"][rr["name"].replace("."+zone_name+".", "")]["txt"].append([rr["txtdata"]])
         elif rr["type"] == "CNAME":
-            the_geozone["data"][rr["name"]]["cname"] = rr["cname"]+"."
+            the_geozone["data"][rr["name"].replace("."+zone_name+".", "")]["cname"] = rr["cname"]+"."
     # Lets write the zone to a JSON file
     with open("/opt/geodns-nDeploy/conf/"+zone_name+".json", 'w') as myzonefile:
         json.dump(the_geozone, myzonefile)
