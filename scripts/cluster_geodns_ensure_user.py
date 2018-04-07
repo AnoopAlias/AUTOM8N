e1450c4709b5c1d8d087aab5dd63f58b62a778c1
Anoop P Alias <anoopalias01@gmail.com>
2018-04-07 16:02:36 +0530

    clean up

--- scripts/cluster_geodns_ensure_user.py
+++ scripts/cluster_geodns_ensure_user.py
@@ -26,7 +26,6 @@ installation_path = "/opt/nDeploy"  # Absolute Installation Path
 # Function defs
 
 def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
