a0c3fb69a7eab790a9a4530c973a2adc562ef1ab
Anoop P Alias <anoopalias01@gmail.com>
2018-05-22 21:20:40 +0530

    Fixes #126

---
+++ scripts/tls_setup_hook_post.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+
+import sys
+import json
+import subprocess
+
+
+__author__ = "Anoop P Alias"
+__copyright__ = "Copyright Anoop P Alias"
+__license__ = "GPL"
+__email__ = "anoopalias01@gmail.com"
+
+
+installation_path = "/opt/nDeploy"  # Absolute Installation Path
+cluster_config_file = installation_path+"/conf/ndeploy_cluster.yaml"
+
+cpjson = json.load(sys.stdin)
+mydict = cpjson["data"]
+domainname = mydict["domain"]
+
+
+with open("/etc/userdatadomains.json", "r") as userdatadomains:
+    json_parsed_userdata = json.load(userdatadomains)
+cpaneluserdata = json_parsed_userdata.get(domainname)
+cpaneluser = cpaneluserdata[0]
+
+subprocess.call("/opt/nDeploy/scripts/generate_config.py "+cpaneluser, shell=True)
+print(("1 nDeploy:WHMTLStrigger:"+cpaneluser))
