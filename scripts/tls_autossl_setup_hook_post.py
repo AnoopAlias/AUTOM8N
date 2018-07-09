915bf5233e341c18672eb5b0380a498e4239194a
Anoop P Alias <anoopalias01@gmail.com>
2018-07-09 10:05:39 +0530

    Better handling of bulk reload

--- scripts/tls_autossl_setup_hook_post.py
+++ scripts/tls_autossl_setup_hook_post.py
@@ -13,6 +13,22 @@ __license__ = "GPL"
 __email__ = "anoopalias01@gmail.com"
 
 
+# Define a function to silently remove files
+def silentremove(filename):
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+
+
+# Define a function to silently add files
+def silentadd(filename):
+    try:
+        os.mknod(filename)
+    except OSError:
+        pass
+
+
 installation_path = "/opt/nDeploy"  # Absolute Installation Path
 cluster_config_file = installation_path+"/conf/ndeploy_cluster.yaml"
 
@@ -30,4 +46,11 @@ if not os.path.isfile('/var/cpanel/mgmt_queue/apache_update_no_restart'):
     subprocess.call("/opt/nDeploy/scripts/generate_config.py "+cpaneluser, shell=True)
     print(("1 nDeploy:WHMTLSAutoSSLtrigger:"+cpaneluser))
 else:
-    print(("1 nDeploy:WHMTLSAutoSSLtrigger:DEFER:"+cpaneluser))
+    silentadd('/opt/nDeploy/conf/skip_nginx_reload')
+    silentadd('/opt/nDeploy/conf/skip_php-fpm_reload')
+    silentadd('/opt/nDeploy/conf/skip_tomcat_reload')
+    subprocess.call("/opt/nDeploy/scripts/generate_config.py "+cpaneluser, shell=True)
+    silentremove('/opt/nDeploy/conf/skip_nginx_reload')
+    silentremove('/opt/nDeploy/conf/skip_php-fpm_reload')
+    silentremove('/opt/nDeploy/conf/skip_tomcat_reload')
+    print(("1 nDeploy:WHMTLSAutoSSLtrigger:DeferReload:"+cpaneluser))
