f298f559154c8ee4c584d9c0553550ef34d1580a
Anoop P Alias <anoopalias01@gmail.com>
2018-07-09 10:49:07 +0530

    inplace upgrade refine

--- scripts/nginx_upgrade_inplace.sh
+++ scripts/nginx_upgrade_inplace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kill -s USR2 $(cat /var/run/nginx.pid)
-sleep 10
+sleep 25
 kill -s WINCH $(cat /var/run/nginx.pid.oldbin)
 kill -s QUIT $(cat /var/run/nginx.pid.oldbin)
 kill -s HUP $(cat /var/run/nginx.pid)
