8e42b52c6891f8c9400154af4038aa97f7e9e040
Anoop P Alias <anoopalias01@gmail.com>
2018-07-15 18:18:36 +0530

    fix path in shell script

--- scripts/attempt_autofix.sh
+++ scripts/attempt_autofix.sh
@@ -2,13 +2,13 @@
 
 # Remove all .conf files if force argument is supplied
 if [ "$#" -eq 1 ] && [ "$1" == "force" ] ; then
-  rm -f /etc/nginx/sites-enabled/*.conf
+  /bin/rm -f /etc/nginx/sites-enabled/*.conf
 fi
 
 ##Attempt to re-generate all nginx config
-touch /opt/nDeploy/conf/skip_nginx_reload
-touch /opt/nDeploy/conf/skip_php-fpm_reload
-touch /opt/nDeploy/conf/skip_tomcat_reload
+/bin/touch /opt/nDeploy/conf/skip_nginx_reload
+/bin/touch /opt/nDeploy/conf/skip_php-fpm_reload
+/bin/touch /opt/nDeploy/conf/skip_tomcat_reload
 
 echo -e '\e[93m Attempting to regenerate all nginx conf  \e[0m'
 for CPANELUSER in $(cat /etc/domainusers|cut -d: -f1);
@@ -23,7 +23,7 @@ if [ ! -f /opt/nDeploy/conf/secure-php-enabled -a -f /var/cpanel/feature_toggles
   echo -e '\e[93m Run "/opt/nDeploy/scripts/init_backends.py autofix" on slaves in XtendWeb cluster \e[0m'
 fi
 
-rm -f /opt/nDeploy/conf/skip_nginx_reload /opt/nDeploy/conf/skip_php-fpm_reload /opt/nDeploy/conf/skip_tomcat_reload
+/bin/rm -f /opt/nDeploy/conf/skip_nginx_reload /opt/nDeploy/conf/skip_php-fpm_reload /opt/nDeploy/conf/skip_tomcat_reload
 
 echo -e '\e[93m Attempting to regenerate  nginx default conf  \e[0m'
 /opt/nDeploy/scripts/generate_default_vhost_config.py
@@ -38,10 +38,10 @@ osversion=$(cat /etc/redhat-release | grep -oE '[0-9]+\.[0-9]+'|cut -d"." -f1)
 echo -e '\e[93m Attempting to restart ndeploy_watcher daemon \e[0m'
 if [ ${osversion} -le 6 ];then
   service ndeploy_watcher stop
-  rm -f /opt/nDeploy/watcher.pid
+  /bin/rm -f /opt/nDeploy/watcher.pid
   service ndeploy_watcher start
 else
   systemctl stop ndeploy_watcher
-  rm -f /opt/nDeploy/watcher.pid
+  /bin/rm -f /opt/nDeploy/watcher.pid
   systemctl start ndeploy_watcher
 fi
