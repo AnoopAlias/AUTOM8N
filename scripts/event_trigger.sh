9450cb6553328de6351e54fe39210182614ea53a
Anoop P Alias <anoopalias01@gmail.com>
2018-07-30 12:11:07 +0530

    clean up event trigger log

--- scripts/event_trigger.sh
+++ scripts/event_trigger.sh
@@ -4,13 +4,13 @@ if [[ $# -ne 3 ]];then
 fi
 if [[ $2 -eq 1 ]]; then
 	if [ $3 = "IN_ATTRIB" ];then
-		/usr/bin/grep "backend_category: PROXY" $1
+		/usr/bin/grep "backend_category: PROXY" $1 > /dev/null
 		if [ $? -ne 0 ];then
 			CPANELUSER=$(stat -c "%U" $1)
 			if [[ $CPANELUSER == root ]];then
 				exit 0
 			else
-				ps aux | grep -v grep | grep "nginx -s reload"
+				ps aux | grep -v grep | grep "nginx -s reload" > /dev/null
 				if [ $? -ne 0 ];then
 					/usr/sbin/nginx -s reload > /dev/null 2>&1
 					echo "$(date) Domain::Stats::Reload ${CPANELUSER}"
