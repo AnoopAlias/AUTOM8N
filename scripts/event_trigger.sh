2f01205a4bfd4d033c03b1b83c3588ed15391fc7
Anoop P Alias <anoopalias01@gmail.com>
2018-07-30 09:41:11 +0530

    cleanup event trigger

--- scripts/event_trigger.sh
+++ scripts/event_trigger.sh
@@ -3,20 +3,25 @@ if [[ $# -ne 3 ]];then
 	exit 1
 fi
 if [[ $2 -eq 1 ]]; then
-	CPANELUSER=$(stat -c "%U" $1)
-	if [[ $CPANELUSER == root ]];then
-		exit 0
-	else
-		if [ $3 = "IN_ATTRIB" ];then
-			ps aux | grep "nginx -s reload"|grep -v grep
-			if [ $? -ne 0 ];then
-				/usr/sbin/nginx -s reload > /dev/null 2>&1
+	if [ $3 = "IN_ATTRIB" ];then
+		/usr/bin/grep "backend_category: PROXY" /opt/nDeploy/domain-data/$1
+		if [ $? -ne 0 ];then
+			CPANELUSER=$(stat -c "%U" $1)
+			if [[ $CPANELUSER == root ]];then
+				exit 0
+			else
+				ps aux | grep -v grep | grep "nginx -s reload"
+				if [ $? -ne 0 ];then
+					/usr/sbin/nginx -s reload > /dev/null 2>&1
+					echo "$(date) Domain::Stats::Reload ${CPANELUSER}"
+				fi
 			fi
-		else
-			echo "$(date) Domain::Data::Modify ${CPANELUSER}"
-			/opt/nDeploy/scripts/generate_config.py $CPANELUSER
-			/usr/sbin/nginx -s reload > /dev/null 2>&1
 		fi
+	else
+		CPANELUSER=$(stat -c "%U" $1)
+		/opt/nDeploy/scripts/generate_config.py $CPANELUSER
+		/usr/sbin/nginx -s reload > /dev/null 2>&1
+		echo "$(date) Domain::Data::Modify ${CPANELUSER}"
 	fi
 else
 	exit 1
