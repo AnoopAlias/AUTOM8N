3e96a081f07a0a15b8cb0d38b0e9116d17e54b9a
Anoop P Alias <anoopalias01@gmail.com>
2018-08-03 08:39:50 +0530

    nginx reload only if reload not going on

--- scripts/reload_nginx.sh
+++ scripts/reload_nginx.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-/usr/sbin/nginx -s reload > /dev/null 2>&1
-echo '1 nDeploy::nginX::reload'
+ps aux | grep -v grep | grep "nginx -s reload" > /dev/null
+if [ $? -ne 0 ];then
+  /usr/sbin/nginx -s reload > /dev/null 2>&1
+  echo '1 nDeploy::nginX::reload'
+fi
