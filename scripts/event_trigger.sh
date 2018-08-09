122dee5d2504680b3bbf454b24536897680a51b0
Anoop P Alias <anoopalias01@gmail.com>
2018-08-09 20:06:46 +0530

    signal instead of nginx -s reload

--- scripts/event_trigger.sh
+++ scripts/event_trigger.sh
@@ -7,17 +7,17 @@ if [[ $2 -eq 1 ]]; then
 		/usr/bin/grep "backend_category: PROXY" $1 > /dev/null
 		if [ $? -ne 0 ];then
 			CPANELUSER=$(stat -c "%U" $1)
-			if [[ $CPANELUSER == root ]];then
+			if [[ ${CPANELUSER} == root ]];then
 				exit 0
 			else
-				/usr/bin/kill -SIGUSR1 $(cat /var/run/nginx.pid)
+				/usr/bin/kill -s SIGUSR1 $(cat /var/run/nginx.pid)
 				echo "$(date) Domain::Stats::Reload ${CPANELUSER}"
 			fi
 		fi
 	else
 		CPANELUSER=$(stat -c "%U" $1)
-		/opt/nDeploy/scripts/generate_config.py $CPANELUSER
-		/usr/bin/kill -SIGHUP $(cat /var/run/nginx.pid)
+		/opt/nDeploy/scripts/generate_config.py ${CPANELUSER}
+		/usr/bin/kill -s SIGHUP $(cat /var/run/nginx.pid)
 		echo "$(date) Domain::Data::Modify ${CPANELUSER}"
 	fi
 else
--- scripts/reload_nginx.sh
+++ scripts/reload_nginx.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-ps aux | grep -v grep | grep "nginx -s reload" > /dev/null
-if [ $? -ne 0 ];then
-  /usr/bin/kill -USR1 $(cat /var/run/nginx.pid)
-  echo '1 nDeploy::nginX::reload'
-fi
+
+/usr/bin/kill -s SIGUSR1 $(cat /var/run/nginx.pid)
+echo '1 nDeploy::nginX::reload'
