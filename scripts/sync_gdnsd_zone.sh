881fc85b0dae9ac43b2a6ad7f7df28e9a4b73956
Anoop Alias <anoop@y510.autom8n.com>
2018-09-04 19:19:22 +0530

    fix script

--- scripts/sync_gdnsd_zone.sh
+++ scripts/sync_gdnsd_zone.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# We proceed further only if its a zone files
-echo $1 | egrep ".db$" > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-  DOMAIN=$(echo $1| awk -F"/" '{print $5}'|sed 's/.db$//')
-  USER=$(/scripts/whoowns ${DOMAIN})
-  /opt/nDeploy/scripts/cluster_gdnsd_ensure_user.py ${USER}
+if [ -f /opt/nDeploy/conf/ndeploy_cluster.yaml ]; then
+  # We proceed further only if its a zone files
+  echo $1 | egrep ".db$" > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    DOMAIN=$(echo $1| awk -F"/" '{print $5}'|sed 's/.db$//')
+    USER=$(/scripts/whoowns ${DOMAIN})
+    /opt/nDeploy/scripts/cluster_gdnsd_ensure_user.py ${USER}
+  fi
 fi
