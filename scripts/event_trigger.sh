04a91f83888dbb456842fb71fd82e7eada9a8696
Anoop Alias <anoop@y510.autom8n.com>
2018-09-25 21:21:15 +0530

    fix hardcoded path being wrong

--- scripts/event_trigger.sh
+++ scripts/event_trigger.sh
@@ -4,20 +4,20 @@ if [[ $# -ne 3 ]];then
 fi
 if [[ $2 -eq 1 ]]; then
 	if [ $3 = "IN_ATTRIB" ];then
-		/usr/bin/grep "backend_category: PROXY" $1 > /dev/null
+		grep "backend_category: PROXY" $1 > /dev/null
 		if [ $? -ne 0 ];then
 			CPANELUSER=$(stat -c "%U" $1)
 			if [[ ${CPANELUSER} == root ]];then
 				exit 0
 			else
-				/usr/bin/kill -s SIGUSR1 $(cat /var/run/nginx.pid)
+				kill -s SIGUSR1 $(cat /var/run/nginx.pid)
 				echo "$(date) Domain::Stats::Reload ${CPANELUSER}"
 			fi
 		fi
 	else
 		CPANELUSER=$(stat -c "%U" $1)
 		/opt/nDeploy/scripts/generate_config.py ${CPANELUSER}
-		/usr/bin/kill -s SIGHUP $(cat /var/run/nginx.pid)
+		kill -s SIGHUP $(cat /var/run/nginx.pid)
 		echo "$(date) Domain::Data::Modify ${CPANELUSER}"
 	fi
 else
