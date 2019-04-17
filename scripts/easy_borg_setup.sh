5faed6567c6207ae1c3e4f2471b411d71062cf16
Anoop Alias <anoop@localhost.localdomain>
2019-04-17 19:27:00 +0530

    fixes

--- scripts/easy_borg_setup.sh
+++ scripts/easy_borg_setup.sh
@@ -2,6 +2,5 @@
 #Author: Anoop P Alias
 
 echo -e '\e[93m Installing borg and borgmatic \e[0m'
-yum --enablerepo=epel -y install borgmatic borgbackup python36-pip
-pip3.6 install 'ruamel.yaml<=0.15'
-pip3.6 install 'pykwalify>=1.6.0'
+yum --enablerepo=epel -y install borgbackup python36 python36-pip
+pip3.6 install borgmatic
