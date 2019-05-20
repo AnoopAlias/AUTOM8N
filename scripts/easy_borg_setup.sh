b6ef6ff2252bb646c1eb161b90c75903bba2fc85
Anoop Alias <anoop@localhost.localdomain>
2019-05-20 19:36:02 +0530

    fix

--- scripts/easy_borg_setup.sh
+++ scripts/easy_borg_setup.sh
@@ -4,4 +4,6 @@
 echo -e '\e[93m Installing borg and borgmatic \e[0m'
 yum --enablerepo=epel -y install borgbackup python36 python36-pip
 pip3.6 install borgmatic
+mkdir /etc/borgmatic
+chmod 750 /etc/borgmatic
 echo -e '\e[93m Setup: Ok \e[0m'
