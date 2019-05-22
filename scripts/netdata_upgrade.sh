6d0242cecf29625c2aaf168394740bac0ab33858
Anoop Alias <anoop@localhost.localdomain>
2019-05-22 11:05:21 +0530

    removed netdata upgrade script

--- scripts/netdata_upgrade.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-#Author: Anoop P Alias
-
-wget -O /root/kickstart-static64.sh https://my-netdata.io/kickstart-static64.sh && bash /root/kickstart-static64.sh --non-interactive
-service netdata restart
