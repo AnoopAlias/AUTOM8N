835f7f039a9a81901f30960edf3ebeafc46903a0
Anoop Alias <anoop@localhost.localdomain>
2019-05-23 20:21:25 +0530

    add MariaDB-backup in borg setup

--- scripts/easy_borg_setup.sh
+++ scripts/easy_borg_setup.sh
@@ -2,7 +2,7 @@
 #Author: Anoop P Alias
 
 echo -e '\e[93m Installing borg and borgmatic \e[0m'
-yum --enablerepo=epel -y install borgbackup python36 python36-pip
+yum --enablerepo=epel -y install borgbackup python36 python36-pip MariaDB-backup
 pip3.6 install borgmatic
 mkdir /etc/borgmatic
 chmod 750 /etc/borgmatic
