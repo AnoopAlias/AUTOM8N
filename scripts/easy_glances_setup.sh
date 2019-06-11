4885fb13d96fffa485c2a8c21bd89d679e2be26e
Anoop Alias <anoop@localhost.localdomain>
2019-06-11 19:59:31 +0530

    add glances setup

---
+++ scripts/easy_glances_setup.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+#Author: Anoop P Alias
+
+
+yum -y remove glances
+yum --enablerepo=epel install python36 python36-pip python36-devel
+pip3 install glances bottle
+
+printf "glances:$(openssl passwd -apr1)" > /etc/nginx/conf.d/glances.password
+chmod 400 /etc/nginx/conf.d/glances.password
+chown nobody /etc/nginx/conf.d/glances.password
+
+systemctl enable ndeploy_glances
+systemctl start ndeploy_glances
+
+if [ -d /opt/nDeploy/conf/nDeploy-cluster ];then
+  /opt/nDeploy/scripts/generate_default_vhost_config.py
+else
+  /opt/nDeploy/scripts/generate_default_vhost_config_slave.py
+fi
+
+nginx -s reload
+
+echo -e "\e[93m You can access glances at https://$(hostname)/netdata with user: glances and password you set \e[0m"
