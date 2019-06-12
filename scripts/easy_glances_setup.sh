fa726b8d7a82453010c27f4e984a32ff11fe217a
Anoop Alias <anoop@localhost.localdomain>
2019-06-12 12:38:13 +0530

    fix wordings

--- scripts/easy_glances_setup.sh
+++ scripts/easy_glances_setup.sh
@@ -3,9 +3,10 @@
 
 which systemctl || exit 1
 yum -y remove glances
-yum --enablerepo=epel install python36 python36-pip python36-devel
+yum -y install python36 python36-pip python36-devel
 pip3 install glances bottle
 
+echo -e "\e[93m Input web user password for glances user below \e[0m"
 printf "glances:$(openssl passwd -apr1)" > /etc/nginx/conf.d/glances.password
 chmod 400 /etc/nginx/conf.d/glances.password
 chown nobody /etc/nginx/conf.d/glances.password
@@ -21,4 +22,4 @@ fi
 
 nginx -s reload
 
-echo -e "\e[93m You can access glances at https://$(hostname)/netdata with user: glances and password you set \e[0m"
+echo -e "\e[93m You can access glances at https://$(hostname)/glances with user: glances and password you set \e[0m"
