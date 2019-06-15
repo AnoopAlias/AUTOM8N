7fa14f5d1b5a7f6eb55417e33a74fd00a1c08585
Anoop Alias <anoop@localhost.localdomain>
2019-06-15 11:00:43 +0530

    fix branding issue

--- nDeploy_whm/commoninclude.py
+++ nDeploy_whm/commoninclude.py
@@ -112,7 +112,7 @@ def print_header():
     print((''))
     print(('<html>'))
     print(('<head>'))
-    print(('<title>AUTOM8N</title>'))
+    print(('<title>'+branding_print_banner()+'</title>'))
     print(('<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>'))
     print(('<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>'))
     print(('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">'))
