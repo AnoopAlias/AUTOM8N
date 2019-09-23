34b8dd1e076b70000e1c13f326191e3e75372fc8
X <xlightwaverx@gmail.com>
2019-09-23 00:51:13 +0000

    HTML5 fix

--- nDeploy_cp/commoninclude.py
+++ nDeploy_cp/commoninclude.py
@@ -160,10 +160,13 @@ def print_loader():
 
 # Print Header
 def print_header(title=''):
-    print('Content-Type: text/html')
     print('')
-    print('<html>')
+    print('<!doctype html>')
+    print('<html lang="en">')
     print('    <head>')
+    print('        <!-- Required meta tags -->')
+    print('        <meta charset="utf-8">')
+    print('        <meta name="viewport" content="width=device-width, initial-scale=1">')
     print('        <title>'+brand+' - '+title+'</title>')
     print('        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>')
     print('        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>')
