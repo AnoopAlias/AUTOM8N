27b5b7c8917bf39e247ca452024a9549cb0f4c59
Anoop Alias <anoop@localhost.localdomain>
2019-10-01 15:17:00 +0530

    add filesync all script

---
+++ scripts/cluster_filesync_ensure_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -e '\e[93m Attempting to sync document roots  \e[0m'
+
+for CPANELUSER in $(cat /etc/domainusers|cut -d: -f1);
+  do
+    echo "FileSycn:: $CPANELUSER" && nice --adjustment=15 /opt/nDeploy/scripts/cluster_filesync_ensure_user.py $CPANELUSER;
+  done
