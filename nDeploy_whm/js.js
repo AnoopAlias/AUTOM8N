ea85b9309121210bf7396f08726189e1a0b220c9
Glyn Gray <glyn@stirstudiosdesign.com>
2019-11-04 19:09:12 -0800

    Updated js validation to search all forms.

--- nDeploy_whm/js.js
+++ nDeploy_whm/js.js
@@ -163,32 +163,28 @@ jQuery(document).ready(function($) {
     });
 
     // General Form Validatons
-    window.addEventListener("load", function() {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName("needs-validation");
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener("submit", function(event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                } else {
-                    var $url = "save_cluster_settings.cgi?" + $(form).serialize();
-                    $.ajax({
-                        url: $url,
-                        success: function(result) {
-                            $.toast({
-                                autohide: 'true',
-                            });
-                            $(".toast-new").toast("show").html(result);
-                            $(".toast").removeClass("toast-new");
+    window.addEventListener(
+        "load",
+        function () {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName("needs-validation");
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function (form) {
+                form.addEventListener(
+                    "submit",
+                    function (event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
                         }
-                    });
-                }
-                form.classList.add("was-validated");
-            }, false);
-        });
-    }, false);
+                        form.classList.add("was-validated");
+                    },
+                    false
+                );
+            });
+        },
+        false
+    );
 
     // Toasts
     $.toast = function(c) {
