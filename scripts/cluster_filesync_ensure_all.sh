debe3e9b48e96a32192b4becb2434463dfa65349
Anoop Alias <anoop@localhost.localdomain>
2019-12-07 07:48:05 +0530

    spell error fix

--- scripts/cluster_filesync_ensure_all.sh
+++ scripts/cluster_filesync_ensure_all.sh
@@ -4,5 +4,5 @@ echo -e '\e[93m Attempting to sync document roots  \e[0m'
 
 for CPANELUSER in $(cat /etc/domainusers|cut -d: -f1);
   do
-    echo "FileSycn:: $CPANELUSER" && nice --adjustment=15 /opt/nDeploy/scripts/cluster_filesync_ensure_user.py $CPANELUSER;
+    echo "FileSync:: $CPANELUSER" && nice --adjustment=15 /opt/nDeploy/scripts/cluster_filesync_ensure_user.py $CPANELUSER;
   done
