40d27216e21ddcccc358e108177de8ef38e2f355
Anoop Alias <anoop@localhost.localdomain>
2020-02-14 12:22:01 +0530

    script

--- scripts/cluster_user_ssh_keyadd.sh
+++ scripts/cluster_user_ssh_keyadd.sh
@@ -1,1 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+if [ -d ~/.ssh ]
+then
+  if [ -f ~/.ssh/clusterkey ]
+  then
+    # Ensure key is in authorized_keys
+    THECLUSTERKEY=$(cat ~/.ssh/clusterkey.pub)
+    grep -w "${THECLUSTERKEY}" .ssh/authorized_keys > /dev/null 2>&1 || cat ~/.ssh/clusterkey.pub >> .ssh/authorized_keys
+    chmod 400 .ssh/authorized_keys
+  else
+    ssh-keygen -b 2048 -t rsa -f ~/.ssh/clusterkey -q -N ""
+    # Ensure key is in authorized_keys
+    THECLUSTERKEY=$(cat ~/.ssh/clusterkey.pub)
+    grep -w "${THECLUSTERKEY}" .ssh/authorized_keys > /dev/null 2>&1 || cat ~/.ssh/clusterkey.pub >> .ssh/authorized_keys
+    chmod 400 .ssh/authorized_keys
+  fi
+else
+  mdir ~/.ssh
+  chmod 400 ~/.ssh
+  ssh-keygen -b 2048 -t rsa -f ~/.ssh/clusterkey -q -N ""
+  # Ensure key is in authorized_keys
+  THECLUSTERKEY=$(cat ~/.ssh/clusterkey.pub)
+  grep -w "${THECLUSTERKEY}" .ssh/authorized_keys > /dev/null 2>&1 || cat ~/.ssh/clusterkey.pub >> .ssh/authorized_keys
+  chmod 400 .ssh/authorized_keys
+fi
