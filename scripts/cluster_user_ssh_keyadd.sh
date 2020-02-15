0f913d5cc31f18cc65300d850155517980dc9089
Anoop Alias <anoop@localhost.localdomain>
2020-02-15 16:26:54 +0530

    fix

--- scripts/cluster_user_ssh_keyadd.sh
+++ scripts/cluster_user_ssh_keyadd.sh
@@ -6,14 +6,18 @@ then
   then
     # Ensure key is in authorized_keys
     THECLUSTERKEY=$(cat ~/.ssh/clusterkey.pub)
-    grep -w "${THECLUSTERKEY}" .ssh/authorized_keys > /dev/null 2>&1 || cat ~/.ssh/clusterkey.pub >> .ssh/authorized_keys
-    chmod 400 .ssh/authorized_keys
+    grep -w "${THECLUSTERKEY}" ~/.ssh/authorized_keys > /dev/null 2>&1 || cat ~/.ssh/clusterkey.pub >> ~/.ssh/authorized_keys
+    chmod 400 ~/.ssh/authorized_keys
+    echo "PasswordAuthentication no" > ~/.ssh/config
+    chmod 400 ~/.ssh/config
   else
     ssh-keygen -b 2048 -t rsa -f ~/.ssh/clusterkey -q -N ""
     # Ensure key is in authorized_keys
     THECLUSTERKEY=$(cat ~/.ssh/clusterkey.pub)
-    grep -w "${THECLUSTERKEY}" .ssh/authorized_keys > /dev/null 2>&1 || cat ~/.ssh/clusterkey.pub >> .ssh/authorized_keys
-    chmod 400 .ssh/authorized_keys
+    grep -w "${THECLUSTERKEY}" ~/.ssh/authorized_keys > /dev/null 2>&1 || cat ~/.ssh/clusterkey.pub >> ~/.ssh/authorized_keys
+    chmod 400 ~/.ssh/authorized_keys
+    echo "PasswordAuthentication no" > ~/.ssh/config
+    chmod 400 ~/.ssh/config
   fi
 else
   mdir ~/.ssh
@@ -21,6 +25,8 @@ else
   ssh-keygen -b 2048 -t rsa -f ~/.ssh/clusterkey -q -N ""
   # Ensure key is in authorized_keys
   THECLUSTERKEY=$(cat ~/.ssh/clusterkey.pub)
-  grep -w "${THECLUSTERKEY}" .ssh/authorized_keys > /dev/null 2>&1 || cat ~/.ssh/clusterkey.pub >> .ssh/authorized_keys
-  chmod 400 .ssh/authorized_keys
+  grep -w "${THECLUSTERKEY}" ~/.ssh/authorized_keys > /dev/null 2>&1 || cat ~/.ssh/clusterkey.pub >> ~/.ssh/authorized_keys
+  chmod 400 ~/.ssh/authorized_keys
+  echo "PasswordAuthentication no" > ~/.ssh/config
+  chmod 400 ~/.ssh/config
 fi
