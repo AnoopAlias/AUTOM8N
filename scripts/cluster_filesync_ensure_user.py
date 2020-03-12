e0575cf4741a0d818730f1826183ee0b7eec1bea
Anoop Alias <anoop@localhost.localdomain>
2020-03-12 18:25:16 +0530

    Fixes #609

--- scripts/cluster_filesync_ensure_user.py
+++ scripts/cluster_filesync_ensure_user.py
@@ -47,4 +47,4 @@ if __name__ == "__main__":
                 subdom_docroot = json_parsed_userdata.get(mydomain)[4]
                 the_raw_cmd = 'ansible -i /opt/nDeploy/conf/nDeploy-cluster/hosts ndeployslaves -m synchronize -a "src='+subdom_docroot+'/ dest='+subdom_docroot+'/"'
                 with open(os.devnull, 'w') as FNULL:
-                    subprocess.call(the_raw_cmd, shell=True)
+                    subprocess.call(the_raw_cmd, stdout=FNULL, shell=True)
