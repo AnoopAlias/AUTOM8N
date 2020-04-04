f00eb6f03eb40c575981eac3a4446230e268afd1
Anoop Alias <anoop@localhost.localdomain>
2020-04-05 00:02:05 +0530

    add cluster keysync via cron

---
+++ scripts/cluster_ssh_keysync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -f /opt/nDeploy/conf/ndeploy_cluster.yaml ];then
+  for CPANELUSER in $(cat /etc/domainusers|cut -d: -f1);
+  do
+    HOMEDIR=$( getent passwd "${CPANELUSER}" | cut -d: -f6 )
+    if [ -d ${HOMEDIR}/.ssh ]; then
+      /usr/bin/ansible -i /opt/nDeploy/conf/nDeploy-cluster/hosts ndeployslaves -m synchronize -a "src=${HOMEDIR}/.ssh/ dest=${HOMEDIR}/.ssh/ rsync_timeout=5"
+    fi
+  done
+fi
