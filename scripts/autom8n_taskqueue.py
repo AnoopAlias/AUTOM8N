c8b921c96d32c12a922ffbae43cceb498d05793a
Anoop Alias <anoop@localhost.localdomain>
2020-07-02 13:38:16 +0530

    initial commit which fixes #345

---
+++ scripts/autom8n_taskqueue.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import celery
+
+
+__author__ = "Anoop P Alias"
+__copyright__ = "Copyright Anoop P Alias"
+__license__ = "GPL"
+__email__ = "anoopalias01@gmail.com"
+
+
+installation_path = "/opt/nDeploy"  # Absolute Installation Path
