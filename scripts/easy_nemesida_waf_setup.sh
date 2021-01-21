e969428a8c2e8fad54b0a17e86c3df7c72eea252
Anoop Alias <anoop@localhost.localdomain>
2021-01-21 18:15:50 +0530

    fix

--- scripts/easy_nemesida_waf_setup.sh
+++ scripts/easy_nemesida_waf_setup.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 #Author: Anoop P Alias
 
+OSVERSION=$(cat /etc/redhat-release | grep -oE '[0-9]+\.[0-9]+'|cut -d"." -f1)
+NGINX_MINOR_VERSION=$(rpm -qa|grep nginx-nDeploy|awk -F"." '{print $2}')
+
+if [ ${OSVERSION} -eq 7 ];then
 rpm -Uvh https://repository.pentestit.ru/nw/centos/nwaf-release-centos-7-1-6.noarch.rpm
 # Keeping the repo disabled by default
 yum-config-manager --disable NemesidaWAF
 export TMPDIR=/root/tmp
-yum -y --enablerepo=NemesidaWAF install nwaf-dyn-1.19
+yum -y --enablerepo=NemesidaWAF install nwaf-dyn-1.${NGINX_MINOR_VERSION}
 yum -y --enablerepo=ndeploy install nginx-nDeploy-module-nemesida
 echo 'Setup: Ok'
+elif [ ${OSVERSION} -eq 8 ];then
+rpm -Uvh https://repository.pentestit.ru/nw/centos/nwaf-release-centos-8-1-6.noarch.rpm
+# Keeping the repo disabled by default
+yum-config-manager --disable NemesidaWAF
+export TMPDIR=/root/tmp
+# Setting up packagecloud.io for rabbitmq-server
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+yum -y --enablerepo=NemesidaWAF install nwaf-dyn-1.${NGINX_MINOR_VERSION}
+yum -y --enablerepo=ndeploy install nginx-nDeploy-module-nemesida
+echo 'Setup: Ok'
+else
+echo "Setup: Failed"
+fi
