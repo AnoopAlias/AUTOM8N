7ce8afd9635aa805fc0611d035eb611c21537b7d
Anoop P Alias <anoopalias01@gmai.com>
2022-01-13 19:23:13 +0530

    playbook changes and cpanel theme related changes

--- scripts/setup_brand.sh
+++ scripts/setup_brand.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo -e '\n<em><strong>Uninstalling previous version from cPanel...</strong></em>'
-/usr/local/cpanel/scripts/uninstall_plugin /opt/nDeploy/nDeploy_cp
+/usr/local/cpanel/scripts/uninstall_plugin /opt/nDeploy/nDeploy_cp --theme paper_lantern
+/usr/local/cpanel/scripts/uninstall_plugin /opt/nDeploy/nDeploy_cp --theme jupiter
 
 echo -e '\n<em><strong>Uninstalling application from WHM...</strong></em>'
 /usr/local/cpanel/bin/unregister_appconfig /opt/nDeploy/nDeploy_whm/xtendweb.conf
@@ -9,7 +10,8 @@ echo -e '\n<em><strong>Uninstalling application from WHM...</strong></em>'
 /opt/nDeploy/scripts/fix_branding.py
 
 echo -e '\n<em><strong>Installing nDeploy plugin in cPanel...</strong></em>'
-/usr/local/cpanel/scripts/install_plugin /opt/nDeploy/nDeploy_cp
+/usr/local/cpanel/scripts/install_plugin /opt/nDeploy/nDeploy_cp --theme paper_lantern
+/usr/local/cpanel/scripts/install_plugin /opt/nDeploy/nDeploy_cp --theme jupiter
 
 echo -e '\n<em><strong>Installing nDeploy plugin in WHM (</strong></em>Registering as XtendWeb<em><strong>)...</strong></em>'
 /usr/local/cpanel/bin/register_appconfig /opt/nDeploy/nDeploy_whm/xtendweb.conf
