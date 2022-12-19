67358dc5ae706f0c7c544f2a51a2aa050698a12b
Anoop P Alias <anoopalias01@gmail.com>
2022-12-19 09:12:54 +0530

    Update cluster_home_ensure_all.py

--- scripts/cluster_home_ensure_all.py
+++ scripts/cluster_home_ensure_all.py
@@ -10,7 +10,7 @@ with open("/etc/domainusers", 'r') as domainusers:
         user_info = pwd.getpwnam(cpaneluser)
         cpaneluserhome = user_info.pw_dir
         cpanelusershell = user_info.pw_shell
-        cpaneluseruid = user_info.pw_uid
-        cpanelusergid = user_info.pw_gid
+        cpaneluseruid = str(user_info.pw_uid)
+        cpanelusergid = str(user_info.pw_gid)
         subprocess.call('ansible -i /opt/nDeploy/conf/nDeploy-cluster/hosts ndeployslaves -m group -a "name='+cpaneluser+' gid='+cpanelusergid+'"', shell=True)
         subprocess.call('ansible -i /opt/nDeploy/conf/nDeploy-cluster/hosts ndeployslaves -m user -a "name='+cpaneluser+' uid='+cpaneluseruid+' group='+cpaneluser+' home='+cpaneluserhome+' shell='+cpanelusershell+'"', shell=True)
