fe0cf6437f82285b13c3d65d21500668cf6d9fd8
Anoop P Alias <anoopalias01@gmail.com>
2024-12-18 18:09:31 +0530

    Delete scripts/cluster_manual_filesync.sh

--- scripts/cluster_manual_filesync.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SSHPORT="22"   #script assumes all slaves have same SSH port settings
-SLAVEHOST="slave1.example.com slave2.example.com"
-
-for MYHOST in ${SLAVEHOST}
-do
-  for dir in $(cat /etc/userdatadomains|awk -F"==" '{print $5}'); do rsync -av -e "ssh -p ${SSHPORT}" ${dir}/ root@${MYHOST}:${dir}/; done
-done
