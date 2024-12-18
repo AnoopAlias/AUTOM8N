c91151895e558467ac3636281044632fef2e4bac
Anoop P Alias <anoopalias01@gmail.com>
2024-12-18 13:51:30 +0530

    account unsuspend restart maxscale

---
+++ scripts/cpanel_functions_hook_post_unsuspend.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+import sys
+import json
+import subprocess
+import os
+from commoninclude import sighupnginx
+
+
+__author__ = "Anoop P Alias"
+__copyright__ = "Copyright Anoop P Alias"
+__license__ = "GPL"
+__email__ = "anoopalias01@gmail.com"
+
+
+installation_path = "/opt/nDeploy"  # Absolute Installation Path
+cluster_config_file = installation_path+"/conf/ndeploy_cluster.yaml"
+
+
+cpjson = json.load(sys.stdin)
+mydict = cpjson["data"]
+cpaneluser = mydict["user"]
+subprocess.call("/opt/nDeploy/scripts/generate_config.py "+cpaneluser, shell=True)  # Assuming escalateprivilege is enabled
+sighupnginx()
+if os.path.exists(cluster_config_file):
+    subprocess.call("systemctl restart maxscale", shell=True)
+    subprocess.call('ansible -i /opt/nDeploy/conf/nDeploy-cluster/hosts ndeploymaster -m shell -a "systemctl restart maxscale"', shell=True)
+    if os.path.isfile(installation_path+"/conf/skip_geodns"):
+        subprocess.call(installation_path + "/scripts/cluster_dns_ensure_user.py "+cpaneluser, shell=True)
+    else:
+        subprocess.call(installation_path + "/scripts/cluster_gdnsd_ensure_user.py "+cpaneluser, shell=True)
+print(("1 nDeploy:cPaneltrigger:"+cpaneluser))
