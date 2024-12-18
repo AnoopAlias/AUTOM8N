d8a0cc7d2e503b52b02a7ae38e1df1473a39df07
Anoop P Alias <anoopalias01@gmail.com>
2024-12-18 15:43:25 +0530

    fix mysql purge logs

--- scripts/purgemysqllogs_dbslave.sh
+++ scripts/purgemysqllogs_dbslave.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mysql -e "SHOW SLAVE STATUS\G"|grep Relay_Master_Log_File|cut -d: -f2|sed 's/ //' > /home/mysql.status
+mysql -e "SHOW SLAVE STATUS\G"|grep Relay_Master_Log_File|cut -d: -f2|sed 's/ //' > /var/lib/maxscale/mysql.status
