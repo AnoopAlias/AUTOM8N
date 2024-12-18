8fa63a19b648413b47fe487a0f6b863dda6d6490
Anoop P Alias <anoopalias01@gmail.com>
2024-12-18 17:49:45 +0530

    new sync all script

---
+++ scripts/cluster_filesync_ensure_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import yaml
+import fileinput
+import sys
+
+
+__author__ = "Anoop P Alias"
+__copyright__ = "Copyright Anoop P Alias"
+__license__ = "GPL"
+__email__ = "anoopalias01@gmail.com"
+
+
+installation_path = "/opt/nDeploy"  # Absolute Installation Path
+
+
+if __name__ == "__main__":
+    if os.path.isfile(installation_path+"/conf/nDeploy-cluster/hosts"):
+        with open(installation_path+"/conf/nDeploy-cluster/hosts", 'r') as hosts_file:
+            yaml_parsed_hosts = yaml.safe_load(hosts_file)
+        for host in yaml_parsed_hosts['all']['children']['ndeployslaves']['hosts'].keys():
+            print(host)
+            ssh_port = yaml_parsed_hosts['all']['children']['ndeployslaves']['hosts'][host].get['ansible_port']
+            subprocess.call('for dir in $(cat /etc/userdatadomains|awk -F"==" '{print $5}'); do rsync -av -e "ssh -p ${'+ssh_port+'}" ${dir}/ root@${'+host+'}:${dir}/; done', shell=True)
