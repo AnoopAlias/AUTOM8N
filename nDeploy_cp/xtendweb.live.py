ffa7bc4e1575357de89cf66e2f185975114f376b
Anoop P Alias <anoopalias01@gmail.com>
2025-02-08 01:23:23 +0530

    retire autoswitch feature.Less clutter

--- nDeploy_cp/xtendweb.live.py
+++ nDeploy_cp/xtendweb.live.py
@@ -135,20 +135,20 @@ print('                <!-- Column Start -->')
 print('                <div class="col-lg-6">')
 
 # Nginx Autoswitcher
-if not os.path.isfile(installation_path + '/conf/autoswitch.disabled'):
-    cardheader('Nginx Autoswitcher','fas fa-cogs')
-    print('                        <div class="card-body">  <!-- Card Body Start -->')
+# if not os.path.isfile(installation_path + '/conf/autoswitch.disabled'):
+#     cardheader('Nginx Autoswitcher','fas fa-cogs')
+#     print('                        <div class="card-body">  <!-- Card Body Start -->')
 
-    if settings_lock == 'enabled':
-        print('                        <div class="text-center alert alert-info">Application Server settings are locked by the administrator</div>')
-    else:
-        print('                            <form class="form" method="post" id="auto_switch_nginx" onsubmit="return false;">')
-        print(('                                <input hidden name="cpaneluser" value="'+cpaneluser+'">'))
-        print('                                <button id="auto-switch-nginx-btn" class="btn btn-outline-primary btn-block" type="submit">Auto Switch To Nginx</button>')
-        print('                            </form>')
+#     if settings_lock == 'enabled':
+#         print('                        <div class="text-center alert alert-info">Application Server settings are locked by the administrator</div>')
+#     else:
+#         print('                            <form class="form" method="post" id="auto_switch_nginx" onsubmit="return false;">')
+#         print(('                                <input hidden name="cpaneluser" value="'+cpaneluser+'">'))
+#         print('                                <button id="auto-switch-nginx-btn" class="btn btn-outline-primary btn-block" type="submit">Auto Switch To Nginx</button>')
+#         print('                            </form>')
 
-    print('                        </div> <!-- Card Body End -->')
-    cardfooter('')
+#     print('                        </div> <!-- Card Body End -->')
+#     cardfooter('')
 
 
 # Cluster manual file sync
