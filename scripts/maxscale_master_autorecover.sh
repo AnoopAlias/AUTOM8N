9180c905389f7b5aa82f4e38cdb8286ef5b8adad
Anoop P Alias <anoopalias01@gmail.com>
2025-03-03 20:08:58 +0530

    Update maxscale_master_autorecover.sh

--- scripts/maxscale_master_autorecover.sh
+++ scripts/maxscale_master_autorecover.sh
@@ -7,9 +7,7 @@ if [ -x /usr/bin/maxctrl ]; then
   cat /tmp/maxctrl_status_master |awk -F"\t" '{print $5}'|awk 'NR == 1'|grep -w "Master, Running" > /dev/null || maxctrl_fail=1
 fi
 if [ $maxctrl_fail -eq 1 ];then
-  mysql --defaults-file=/root/.my.cnf -e "STOP SLAVE;"
-  ansible -i /opt/nDeploy/conf/nDeploy-cluster/hosts ndeploydbslave -a "/usr/bin/systemctl stop mariadb"
-  sleep 10
-  ansible -i /opt/nDeploy/conf/nDeploy-cluster/hosts ndeploydbslave -a "/usr/bin/systemctl start mariadb"
-  mysql --defaults-file=/root/.my.cnf -e "START SLAVE;"
+  mariadb --defaults-file=/root/.my.cnf -e "STOP SLAVE;"
+  sleep 5
+  mariadb --defaults-file=/root/.my.cnf -e "START SLAVE;"
 fi
