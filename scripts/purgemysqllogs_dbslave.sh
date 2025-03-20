5c9f729ceef25e99b2f4ff272dae65f893635800
Anoop P Alias <anoopalias01@gmail.com>
2025-03-20 07:35:54 +0530

    Update purgemysqllogs_dbslave.sh

--- scripts/purgemysqllogs_dbslave.sh
+++ scripts/purgemysqllogs_dbslave.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-myariadb -e "SHOW SLAVE STATUS\G"|grep Relay_Master_Log_File|cut -d: -f2|sed 's/ //' > /var/lib/maxscale/mysql.status
+mariadb -e "SHOW SLAVE STATUS\G"|grep Relay_Master_Log_File|cut -d: -f2|sed 's/ //' > /var/lib/maxscale/mysql.status
