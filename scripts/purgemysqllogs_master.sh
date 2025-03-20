a73fc5aa7ca50bc6b75e3e0f8b0fbed686fe28f5
Anoop P Alias <anoopalias01@gmail.com>
2025-03-20 07:36:34 +0530

    Update purgemysqllogs_master.sh

--- scripts/purgemysqllogs_master.sh
+++ scripts/purgemysqllogs_master.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ansible -i /opt/nDeploy/conf/nDeploy-cluster/hosts ndeploydbslave -m fetch -a "src=//var/lib/maxscale/mysql.status dest=/var/lib/maxscale/mysql.status flat=yes"
-myariadb -e "PURGE BINARY LOGS TO '$(cat /var/lib/maxscale/mysql.status)';" 2> /dev/null
+mariadb -e "PURGE BINARY LOGS TO '$(cat /var/lib/maxscale/mysql.status)';" 2> /dev/null
