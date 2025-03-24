1b8bd3eaac494160fe723800c5723510ada089c4
Anoop P Alias <anoopalias01@gmail.com>
2025-03-24 09:09:42 +0530

    Delete scripts/cluster_filesync_ensure_all.sh

--- scripts/cluster_filesync_ensure_all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e '\e[93m Attempting to sync document roots  \e[0m'
-
-for CPANELUSER in $(cat /etc/domainusers|cut -d: -f1);
-  do
-    echo "FileSync:: $CPANELUSER" && nice --adjustment=15 /opt/nDeploy/scripts/cluster_filesync_ensure_user.py $CPANELUSER;
-  done
