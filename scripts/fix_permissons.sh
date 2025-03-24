3ddbd7aeb13097979d46cfe4575d8b4087d2399a
Anoop P Alias <anoopalias01@gmail.com>
2025-03-24 10:50:38 +0530

    Update fix_permissons.sh

--- scripts/fix_permissons.sh
+++ scripts/fix_permissons.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for dir in $(cat /etc/userdatadomains|awk -F"==" \'{print $5}\')
+for dir in $(cat /etc/userdatadomains|awk -F"==" '{print $5}')
 do
   echo "*** Fixing permissions for document_root ${dir} ***"
   find "${dir}"/ -type f -exec chmod 644 {} \;
