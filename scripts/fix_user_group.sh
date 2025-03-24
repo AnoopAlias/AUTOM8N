ce8fe20ef2dffff3a5a991771429fce6774de24c
Anoop P Alias <anoopalias01@gmail.com>
2025-03-24 10:23:43 +0530

    Create fix_user_group.sh

---
+++ scripts/fix_user_group.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CPANELUSER="${1}"
+HOMEDIR=$( getent passwd "${CPANELUSER}" | cut -d: -f6 )
+if [ -d ${HOMEDIR} ]; then
+  echo "*** Fixing ownership and group of account ${CPANELUSER} ***"
+  
+  find /home/${user}/ -not \( -group ${user} -o -group nobody -o -group mail  \) -print -exec chgrp ${user} {} \;
+  find /home/${user}/ -not -user ${user} -print -exec chown chgrp ${user} {} \;
+
+  echo "***  Done..   ***"
